7687d6f4246aea37f066a327303c26579c149ca8
waddella <adrian.waddell@roche.com>
2017-03-16 17:13:58 +0100

    warning message in add filter variables box when trying to add variables of class "unknown"

--- R/ui_add_filter_variable.R
+++ R/ui_add_filter_variable.R
@@ -3,7 +3,8 @@ ui_add_filter_variable <- function(id, dataname) {
   ns <- NS(id)
 
   div( class = paste0("teal_filter_", tolower(dataname)),
-    selectInput(ns("variables"), label=dataname , choices=NULL)
+    selectInput(ns("variables"), label=dataname , choices=NULL),
+    uiOutput(ns("warning"))
   )
 
 
