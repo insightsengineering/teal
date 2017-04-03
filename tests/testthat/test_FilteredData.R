b47c5371045eff34b81dc28bacdd145cd4f9cdb7
waddella <adrian.waddell@roche.com>
2017-04-03 14:57:50 +0200

    update tests for FilteredData Class to be now case sensitive for data set names.

--- tests/testthat/test_FilteredData.R
+++ tests/testthat/test_FilteredData.R
@@ -4,21 +4,21 @@ context("FilteredData")
 
 options(teal_logging = FALSE)
 
-x <- teal:::FilteredData$new(datanames = c('asl', 'atr'))
+x <- teal:::FilteredData$new(datanames = c('ASL', 'atr'))
 
 test_that(
   "Initialization is correct",
-  expect_identical(x$datanames(), c('asl', 'atr'))
+  expect_identical(x$datanames(), c('ASL', 'atr'))
 )
 
 
 
 ASL <- generate_sample_data("ASL")
-x$set_data("asl", ASL)
+x$set_data("ASL", ASL)
 
 test_that(
   "load and set_datasets",
-  expect_identical(x$get_data('asl'), ASL)
+  expect_identical(x$get_data('ASL'), ASL)
 )
 
 
