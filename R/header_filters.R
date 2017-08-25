11c9cacb48c018bd1cf264e336be669c2835b613
waddella <adrian.waddell@roche.com>
2017-08-25 12:59:02 +0200

    refactor output_header and get_filter_txt

--- R/header_filters.R
+++ R/header_filters.R
@@ -4,95 +4,123 @@
 #' @param description A character description of the module with additional
 #'   information not reflected in the title
 #' @param libraries A character vector of the names of libraries required for
-#'   the module. Defaults to c("ggplot2", "haven", "dplyr")
+#'   the module.
 #' @param git_repo A character vector of the link to the GitHub repository
-#'   containing the script for the module. Optional argument
-#' @param data A character vector listing the code for data variable assignments,
-#'   one for each dataset and with the file paths to the datasets
-#'   (e.g. data = "ASL <- haven::read_sas("/opt/BIOSTAT/qa/cdt7876a/libraries/asl.sas7bdat")").
-#'   Note that if any of the dataset paths are not found under /opt/BIOSTAT/...,
-#'   then \code{output_header} would not generate the text corresponding to the
-#'   code for reading in data, and user would need to manually add it within
-#'   the module code. Defaults to c(), or no data lines generated
+#'   containing the R-code for the teal module. Optional argument
+#' @param data optionally either an FilteredData object, or a named list of
+#'   datasets. The data sets need the \code{import} attribute and optionally the
+#'   \code{md5sum} attribute.
 #'
 #' @return A character string for the header text
 #'
 #' @export
 #'
-output_header <- function(title, description, libraries = c("ggplot2", "haven", "dplyr"), git_repo = NULL, data = c()) {
+#' @examples
+#'
+#' cat(output_header(
+#'  title = "Cross-Table",
+#'  description = "A cross-table counts ..."
+#' ))
+#'
+#' cat(output_header(
+#'  title = "Cross-Table",
+#'  description = "A cross-table counts ...",
+#'  libraries = c('haven', 'ggplot2', 'dplyr')
+#' ))
+#'
+#' cat(output_header(
+#'  title = "Cross-Table",
+#'  description = "A cross-table counts ...",
+#'  libraries = c('haven', 'ggplot2', 'dplyr'),
+#'  git_repo = "http://github.roche.com/Rpackages/teal/",
+#'  data = list(
+#'    ASL = structure(data.frame(a = 1), import = "haven::read_sas('/opt/BIOSTAT/asl.sas7bdat')"),
+#'    ATE = structure(data.frame(a = 1),
+#'                    import = "haven::read_sas('/opt/BIOSTAT/ate.sas7bdat')",
+#'                    md5sum = "32sdf32fds324")
+#'  )
+#' ))
+#'
+output_header <- function(title, description=NULL, libraries = NULL, git_repo = NULL, data = NULL) {
 
-  descrip_str <- strsplit(description, "\n")[[1]] # In case of multi-line descriptions
+  descrip_str <- description # unlist(strsplit(description, "\n")) # In case of multi-line descriptions
 
   source_str <- ifelse(is.null(git_repo), "Not given", git_repo)
 
-  if (dir.exists(file.path(getwd(), ".git"))) {
-    git_commit <- system("git rev-parse --short HEAD", intern = TRUE)
-  }
+  git_commit <- try(system("git rev-parse --short HEAD", intern = TRUE), silent = TRUE)
+  if (is(git_commit, "try-error")) git_commit <- ""
 
-  date <- date()
-
-  lib_str <- if (is.null(libraries)) { "" } else {
+  lib_str <- if (!is.null(libraries)) {
     paste0("library(", libraries, ")", collapse = "\n")
-  }
-
-  if (!is.null(data) & all(grepl("/opt/BIOSTAT/", data))) {
-
-    # Need paths for checksums
-    paths <- sub("(.*)[/]opt[/]BIOSTAT", "/opt/BIOSTAT", data)
-    paths <- sub("[\"][)]$", "", paths)
-
-    checksums <- tools::md5sum(paths)
-    checksums_str <- paste0("  # MD5 checksum: ", checksums)
-
-    data_str <- vapply(data, function(x) sub("=", "<-", x), character(1), USE.NAMES = FALSE)
-    data_str <- paste0(data_str, checksums_str, collapse = "\n") # Add checksums as comments
-  }
-
-  if (exists("git_commit")) {
-    paste(
-      paste0("\n# ", title, "\n"),
-      paste0(paste0("# ", descrip_str), collapse = "\n"),
-      paste0("# Source: ", source_str),
-      paste0("# Git-commit: ", git_commit),
-      paste0("# Date: ", date),
-      "#\n# You can run this code interactively in http://r.roche.com \n",
-      paste0(lib_str, "\n"),
-      if (exists("data_str")) data_str,
-      sep = "\n"
-    )
   } else {
-    paste(
-      paste0("\n# ", title, "\n"),
-      paste0(paste0("# ", descrip_str), collapse = "\n"),
-      paste0("# Source: ", source_str),
-      paste0("# Date: ", date),
-      "#\n# You can run this code interactively in http://r.roche.com \n",
-      paste0(lib_str, "\n"),
-      if (exists("data_str")) data_str,
-      sep = "\n"
-    )
+    ""
   }
+
+  # get data import strings
+  if (is(data, "FilteredData")) {
+    data <- lapply(data$datanames(), function(dn) datasets$get_data(dn, filtered = FALSE, reactive = FALSE))
+  }
+
+  if (any(!vapply(data, is.data.frame, logical(1)))) {
+    stop("data needs to be either a FilteredData object or a list of data.frames")
+  }
+
+  if (any(vapply(data, function(df) is.null(attr(df, "import")), logical(1)))) {
+    stop("data needs import attribute")
+  }
+
+  data_str <- paste(Map(function(dt, name) {
+    md5 <- attr(dt, "md5sum")
+    import <- attr(dt, "import")
+
+    md5_str <- if (!is.null(md5)) {
+      paste("# md5sum at time of analysis:", md5)
+    } else {
+      ""
+    }
+    paste0(name, " <- ", import, "  ",  md5_str)
+  }, data, names(data)), collapse = "\n")
+
+  pad <- function(str, pre="", post = "\n\n") {
+    if (is.null(str) || length(str) == 0 || str == "") str else paste0(pre, str, post)
+  }
+
+  commented <- gsub("\n", "\n# ", paste(c(
+    paste("#", title), "\n\n",
+    pad(description),
+    pad(paste("Module Source:", source_str), post="\n"),
+    pad(paste("Git-commit Shiny App:", git_commit), post="\n"),
+    pad(paste("Date:", date()), post = "\n"),  "\n",
+    "You can run this code interactively in http://r.roche.com"
+  ), collapse = ""), fixed=TRUE)
+
+  paste(c(pad(commented), pad(lib_str), data_str), collapse = "")
+
 }
 
 #' @title Generates text for the code to filter datasets
 #'
-#' @param datasets datasets used for the teal module
+#' @param datanames vector with data names
+#' @param datasets datasets object
 #'
 #' @return A character string for the code to generate the filtered datasets
 #'
 #' @export
 #'
-get_filter_txt <- function(datasets) {
+get_filter_txt <- function(datanames, datasets) {
 
-  datasets$get_filter_state("ASL", reactive = TRUE)
-  datasets$get_filter_state("ARS", reactive = TRUE)
-  str_filter_calls <- lapply(datasets$get_filter_call("ARS", merge = TRUE), deparse)
+  if (any(!(datanames %in% datasets$datanames()))) {
+    sel <- !(datanames %in% datasets$datanames())
+    stop("datanames:", paste(datanames[sel], collapse = ", "), "are not available in datasets")
+  }
 
-  paste(c(
-    "# Filter Data",
-    str_filter_calls[[1]], "",
-    str_filter_calls[[2]],
-    str_filter_calls[[3]]
-  ), collapse = "\n")
+  dnames <- c("ASL", setdiff(datanames, "ASL"))
 
+
+  calls <-  lapply(dnames, function(dn) {
+    paste(deparse(datasets$get_filter_call(dn, merge = TRUE, asl=FALSE), width.cutoff = 80), collapse = "\n")
+  })
+
+
+  paste(calls, collapse = "\n")
 }
