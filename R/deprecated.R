1f462f9d167e7ea75a097022bcd0a9f2473689d4
waddella <adrian.waddell@roche.com>
2017-09-05 15:13:24 +0200

    move deprecated function to the deprecated.R file

--- R/deprecated.R
+++ R/deprecated.R
@@ -17,3 +17,28 @@ tabs <- function(...) {
   warning("tabs is deprecated, please use modules()")
   modules(...)
 }
+
+#' Deprecated: Variable Browser Teal Module
+#'
+#' Please use the \code{\link{tm_variable_browser}} function instead.
+#'
+#' @param ... arguments passed on to \code{\link{tm_variable_browser}}
+#'
+#' @export
+variable_browser_item <- function(...) {
+  warning("variable_browser_item is deprecated, pleas use tm_variable_browser instead")
+  tm_variable_browser(...)
+}
+
+
+#' Deprecated: Data Table Viewer Teal Module
+#'
+#' Please use the \code{\link{tm_data_table}} function instead.
+#'
+#' @param ... arguments passed on to \code{\link{tm_data_table}}
+#'
+#' @export
+data_table_item <- function(...) {
+  warning("data_table_item is deprecated, pleas use data_table instead")
+  tm_data_table(...)
+}
