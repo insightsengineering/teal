82116bc517053cd0dde9f3894d71ee12136e01df
waddella <adrian.waddell@roche.com>
2017-09-19 19:42:02 +0200

    initial commit

---
+++ utils.R
@@ -0,0 +1,38 @@
+
+#' Check if list or data.frame has elements/variables
+#' 
+#' Checks if list names exist and throws an error otherwise
+#' 
+#' @param data a \code{data.frame} or named list
+#' @param names vector with names
+#' 
+#' @return \code{TRUE} if all variables exist and an appropriate error if not.
+#' 
+#' @author Adrian Waddell (waddella), \email{adrian.waddell@roche.com}
+#' 
+#' @examples 
+#' \dontrun{
+#' # function is not exported
+#' `%needs%` <- teal.oncology:::`%needs%`
+#' 
+#' iris %needs% c("Sepal.Length", "Petal.Width")
+#' 
+#' iris %needs% "ABC"
+#' 
+#' }
+#' 
+`%needs%` <- function(data, names) {
+  
+  i <- is.na(match(names, names(data)))
+
+  if (any(i)) {
+    msg <- if (sum(i) == 1) {
+      paste("variable ", names[i], " does not exist")
+    } else {
+      paste("variables", paste(names[i], collapse = ", "), "do not exist")
+    }
+    stop(msg)
+  }
+  
+  invisible(TRUE)
+}
