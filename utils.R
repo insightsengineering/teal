2415dd1f569f67de0ca694c8e436ce3c740aa45f
wangh107 <wangh107@gene.com>
2017-09-20 19:51:06 -0700

    add some utility functions

--- utils.R
+++ utils.R
@@ -36,3 +36,50 @@
   
   invisible(TRUE)
 }
+
+
+
+
+
+
+#' Replace empty string and "." as NA in a vector
+#' 
+#' Convert "", "   " and "." to NA in a vector
+#' 
+#' @param input.vec  input vector
+#' 
+#' @export
+#' 
+#' @importFrom stringr str_trim
+#' 
+NaReplaceVec <- function(input.vec){
+  ifelse(is.na(input.vec), NA, 
+         ifelse(!(str_trim(input.vec) %in% c("", ".")), input.vec, NA))
+}
+
+
+#' Replace empty string as NA in a data table
+#' 
+#' Convert factor to character, convert empty string and "." for all columns of a data table
+#' 
+#' @param data.tbl input data table
+#' 
+#' @export
+#' 
+#' @import dplyr
+#' 
+NaReplace <- function(data.tbl){    
+  if (!(is.tbl(data.tbl))){
+    data.tbl <- as_tibble(data.tbl)
+  }
+  for (vec in colnames(data.tbl)){
+    if (data.tbl[ , vec] %>% pull() %>% is.factor()){
+      data.tbl[ , vec] <- data.tbl %>% mutate_at(vec, as.character) %>% mutate_at(vec, NaReplaceVec)
+    }else if (data.tbl[ , vec] %>% pull() %>% is.numeric()){
+      data.tbl[ , vec] <- data.tbl[ , vec]
+    }else{
+      data.tbl[ , vec] <- data.tbl %>% mutate_at(vec, NaReplaceVec)
+    }
+  }
+  return(data.tbl)
+}
