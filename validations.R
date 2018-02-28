596f0611289e4c2793bc0381b6442dac28d99473
waddella <adrian.waddell@roche.com>
2018-02-28 17:57:28 +0100

    finish tm_t_rsp

--- validations.R
+++ validations.R
@@ -13,3 +13,16 @@ validate_has_data <- function(x, min_nrow = NULL) {
 validate_one_row_per_id <- function(x, key = c("USUBJID", "STUDYID")) {
   validate(need(!any(duplicated(x[key])) , paste("more then one row per id")))
 }
+
+validate_in <- function(x, choices, msg) {
+  validate(need(length(x) > 0 && length(choices) > 0  && all(x %in% choices), msg))  
+}
+
+validate_has_elements <- function(x, msg) {
+  validate(need(length(x) > 0, msg))
+}
+
+validate_no_intersection <- function(x, y, msg) {
+  validate(need(length(intersect(x, y)) == 0, msg))
+}
+
