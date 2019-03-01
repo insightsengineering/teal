1388d48ecaa0b8f10656100b7a35c950abd99a87
stoilovs <jana.stoilova@roche.com>
2019-03-01 10:37:09 -0500

    Add select argument to named_choices

--- R/named_choices.R
+++ R/named_choices.R
@@ -5,6 +5,9 @@
 #' 
 #' @param choices a character vector
 #' @param labels vector containing labels to be applied to \code{choices}
+#' @param select a character vector that is a subset of \code{choices}. This is useful if 
+#' only a few variables need to be named. If this argument is used, the returned vector will 
+#' match it's order.
 #' 
 #' @details If either \code{choices} or \code{labels} are factors, they are coerced 
 #'   to character.
@@ -27,10 +30,9 @@
 #' choices1 <- named_choices(names(ADSL), tern::var_labels(ADSL))
 #' choices2 <- named_choices(ADTTE$PARAMCD, ADTTE$PARAM)
 #' 
-#' # if only a subset of variables are of intereste then the following code will work
-#' vl_ADSL <- tern::var_labels(ADSL)
-#' vl_arm <- vl_ADSL[c("ARM", "ARMCD")]
-#' choices3 <- named_choices(names(vl_arm), vl_arm)
+#' # if only a subset of variables are needed, use select argument
+#' choices3 <- named_choices(names(ADSL), tern::var_labels(ADSL), 
+#'                          select = c("ARMCD", "ARM"))
 #' 
 #' 
 #' \dontrun{
@@ -44,14 +46,18 @@
 #'                  
 #'                  selectInput("c2", label = "Choices from ADTTE",
 #'                              choices = choices2,
-#'                              selected = choices2[1])),
+#'                              selected = choices2[1]),
+#'                              
+#'                  selectInput("c3", label = "Arm choices from ADSL",
+#'                              choices = choices3,
+#'                              selected = choices3[1])),
 #'   
 #'   server = function(input, output) {}
 #' )
 #' 
 #' }
 
-named_choices <- function(choices, labels){
+named_choices <- function(choices, labels, select=NULL){
   
   if(is.factor(choices)){
     choices <- as.character(choices)
@@ -62,11 +68,30 @@ named_choices <- function(choices, labels){
   }
   
   stopifnot(is.character(choices))
+  
   length(choices) == length(labels) || stop("length of choices must be the same as labels")
   
-  is_dupl <- duplicated(choices)
+  stopifnot(is.null(select) || is.character(select))
   
-  stats::setNames(choices[!is_dupl], paste0(choices[!is_dupl], ": ", labels[!is_dupl]))
+  if(is.character(select)){
+    all(select %in% choices) || stop("all of select variables must be in choices")
+    
+    #subset choices + labels based on variables in select
+    labels <- labels[choices %in% select]
+    choices <- choices[choices %in% select]
+  }
+  
+  is_dupl <- duplicated(choices)
+  choices <- choices[!is_dupl]
+  labels <- paste0(choices[!is_dupl], ": ", labels[!is_dupl])
+    
+  if(is.character(select)){
+    ord <- match(choices, select)
+    choices <- choices[ord]
+    labels <- labels[ord]
+  }  
+  
+  stats::setNames(choices, labels)
 }
 
 
