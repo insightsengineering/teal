1f00efd0a98bad29a9ae9c62e4bdc5bb287c752d
Kaledkowski <kaledkod@emea.roche.com>
2019-03-15 17:14:19 +0100

    moved functions to teal.devel

--- man-roxygen/descr_hidden_input.R
+++
@@ -1,4 +0,0 @@
-#' @description
-#' Hidden input widgets are useful to have the \code{input[[inputId]]} variable
-#' on available in the server funciont but no corresponding visual clutter from
-#' input widgets that provide only a single choice.
