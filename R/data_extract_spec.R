56290039c6c43855c3eb5cfa31cf2cf5d78480af
wolfs25 <sebastian.wolf.sw1@roche.com>
2019-03-18 18:35:46 +0000

    #1 reworked documentation and fixed a bug in get_data_extracted

--- R/data_extract_spec.R
+++ R/data_extract_spec.R
@@ -1,23 +1,28 @@
 #' Data Extract input for teal modules
 #'
 #' The Data Extract input can be used to filter and select columns from a data
-#' set to be used in teal. From this function's output a \code{data_extract_input} can
+#' set to be used in teal. Please use the constructor function \link{data_extract_spec}
+#' to set it up.
+#'
+#' From this function's output a \code{\link{data_extract_input}} can
 #' be constructed. This input can be read by a \link{data_extract_module} module.
 #'
 #' @export
 #' @name DataExtractSpec
-#' @aliases data_extract_class data_extract_spec
-#' 
-#' @param dataname (\code{character}) Name of a teal data set
-#' @param keys_filtering (\code{list}) Define how to filter the
-#'  key columns of the data set. This is the outcome of \link{keys_filtering_spec}
-#' @param columns (\code{choices_selected}) Define which columns of the data set shall
-#'  be selected next to the key variables. This shall be the outcome of \link{choices_selected}
+#' @keywords data
+#' @docType class
+#' @section Initialize:
+#' \describe{
+#'   \item{via new}{ \code{DataExtractSpec$new(dataname, keys_filtering = NULL, columns = NULL)} }
+#'   \item{via constructor}{
+#'    \code{\link{data_extract_spec}(dataname, keys_filtering = NULL, columns = NULL)}
+#'   }
+#' }
 #'
 #' @field dataname (\code{character}) Data set to be extracted and selected
-#' @field keys_filtering (\code{KeysFilteringSpec}) Setup of the dataset filtering
-#' @field columns (\code{choices_selected}) Columns to be selected from the input dataset
-data_extract_class <- R6::R6Class("DataExtractSpec",
+#' @field keys_filtering (\code{\link{KeysFilteringSpec}}) Setup of the dataset filtering
+#' @field columns (\code{\link{choices_selected}}) Columns to be selected from the input dataset
+DataExtractSpec <- R6::R6Class("DataExtractSpec", # nolint
   public = list(
     dataname = character(0),
     keys_filtering = NULL,
@@ -41,10 +46,14 @@ data_extract_class <- R6::R6Class("DataExtractSpec",
 
 #' Constructor for \link{DataExtractSpec}
 #'
-#' @inheritParams DataExtractSpec
-#' @rdname DataExtractSpec
+#' @param dataname (\code{character}) Name of a teal data set
+#' @param keys_filtering (\code{list}) Define how to filter the
+#'  key columns of the data set. This is the outcome of \link{keys_filtering_spec}
+#' @param columns (\code{choices_selected}) Define which columns of the data set shall
+#'  be selected next to the key variables. This shall be the outcome of \link{choices_selected}
+#'
+#' @return \link{DataExtractSpec} class object
 #' @export
 data_extract_spec <- function(dataname = NULL, keys_filtering = NULL, columns = NULL) {
-  data_extract_class$new(dataname = dataname, keys_filtering = keys_filtering, columns = columns)
+  DataExtractSpec$new(dataname = dataname, keys_filtering = keys_filtering, columns = columns)
 }
-
