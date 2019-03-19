6fc99f3f01cb848a018ae9f2828da24a21880c3c
Maximilian Mordig <maximilian.mordig@gmail.com>
2019-03-19 15:56:20 +0000

    check for different filterings should be working now

--- R/get_selected_columns.R
+++ R/get_selected_columns.R
@@ -1,21 +1,34 @@
+full_dataset_column_identifier <- function(data, columns) {
+  dataname <- attr(data, "dataname")
+  stopifnot(is.atomic(dataname) && (length(dataname) == 1))
+  paste(dataname, columns, sep = ".")
+}
+
 get_non_key_columns <- function(data) {
   keys <- attr(data, "keys")
   setdiff(names(data), keys)
 }
 
 #' Derive non-key column names from data
-#'
+#' 
+#' Called by the user writing functions like tm_plot_xy
+#' 
 #' @export
 #'
 #' @param data (\code{data.frame}) Data with attribute \code{keys} and \code{dataname}
+#' @param remove_rowid whether to remove rowid that was added during filtering (should only be set to FALSE internally)
 #'
 #' @return A named character vector with <variable> ---> <dataname>.<variable>
 #'
-get_dataset_prefixed_col_names <- function(data) {
+get_dataset_prefixed_col_names <- function(data, remove_rowid=TRUE) {
   keys <- attr(data, "keys")
   non_key_columns <- setdiff(names(data), keys)
+  stopifnot("rowid" %in% non_key_columns)
+  if (remove_rowid) {
+    non_key_columns <- setdiff(non_key_columns, "rowid")
+  }
 
-  new_column_names <- paste(attr(data, "dataname"), non_key_columns, sep = ".")
+  new_column_names <- full_dataset_column_identifier(data, non_key_columns)
   names(new_column_names) <- non_key_columns
   return(new_column_names)
 }
@@ -36,9 +49,4 @@ get_nonkey_col_values <- function(data) {
   data %>% dplyr::select(non_key_columns)
 }
 
-# prefixes non-key columns with dataset name
-prefix_col_names_with_dataset <- function(data) {
-  overwrite_names <- get_dataset_prefixed_col_names(data)
-  names(data)[which(names(data) %in% names(overwrite_names))] <- overwrite_names
-  return(data)
-}
+
