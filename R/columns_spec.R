6e7220af5cd1242fc8329193b33f515d4e82660c
wolfs25 <sebastian.wolf.sw1@roche.com>
2019-03-21 18:27:58 +0000

    #10 reworked Pawels comments (R-code)

--- R/columns_spec.R
+++ R/columns_spec.R
@@ -1,5 +1,5 @@
 # Instead of column_filter overwrite choices_selected
-# backwards compatible with new arguments show and label
+# backwards compatible with new arguments fixed and label
 #' column_choices_spec
 #'
 #' @param choices (\code{character}) Named character vector to define the choices
@@ -8,20 +8,19 @@
 #'  values of a shiny select input.
 #' @param multiple (\code{logical}) Whether multiple values shall be allowed in the
 #'  shiny select input.
-#' @param show (\code{logical}) (optional) \link{DataExtractSpec} specific feature to
+#' @param fixed (\code{logical}) (optional) \link{DataExtractSpec} specific feature to
 #'   hide the choices selected in case they are not needed.
 #' @param label (\code{logical}) (optional) \link{DataExtractSpec} specific feature to
-#'   show a different label on top of this specific \link{shiny}{selectInput}.
-#' @param ... additional parameters
+#'   fixed a different label on top of this specific \link{shiny}{selectInput}.
 #'
 #' @return A \code{list} of all input values. The function double checks the \code{choices}
 #'   and \code{selected} inputs.
 #'
 #' @importFrom magrittr %<>%
-#'
-#'
+#' @importFrom purrr map_lgl
+#' @importFrom stats setNames
 #' @export
-columns_spec <- function(choices, selected, multiple, show = TRUE, label = "Column(s)", ...) {
+columns_spec <- function(choices, selected, multiple, fixed = TRUE, label = "Column(s)") {
   # when choices and selected is not a list, we convert it to a list (because each
   # entry is an atomic vector of possibly several entries, needed for filter_spec currently)
   choices <- as.list(choices)
@@ -30,17 +29,13 @@ columns_spec <- function(choices, selected, multiple, show = TRUE, label = "Colu
   stopifnot(is.list(selected) && length(selected) >= 1 && all_true(selected, is.atomic))
   stopifnot(all(selected %in% choices)) # selected and choices must be a list to work correcty
   stopifnot(is.logical(multiple))
-  stopifnot(is.logical(show))
+  stopifnot(is.logical(fixed))
   stopifnot(is.character(label) && length(label) == 1)
   # check for correct lengths
   stopifnot(multiple || length(selected) == 1)
-  length_selected <- length(selected[[1]])
-  has_same_length <- function(elem) {
-    length(elem) == length_selected
-  }
-  stopifnot(all_true(choices, has_same_length))
-  stopifnot(all_true(selected, has_same_length))
 
+  stopifnot(all(map_lgl(selected, ~length(.) == length(selected[[1]]))))
+  stopifnot(all(map_lgl(choices, ~length(.) == length(choices[[1]]))))
   # if names is NULL, shiny will put strange labels (with quotes etc.) in the selectInput, so we set it to the values
   if (is.null(names(choices))) {
     choices %<>% setNames(choices)
@@ -48,7 +43,7 @@ columns_spec <- function(choices, selected, multiple, show = TRUE, label = "Colu
   if (is.null(names(selected))) {
     selected %<>% setNames(selected)
   }
-  res <- list(choices = choices, selected = selected, multiple = multiple, show = show, label = label, ...)
+  res <- list(choices = choices, selected = selected, multiple = multiple, fixed = fixed, label = label)
   class(res) <- "column_choices_spec"
   res
 }
