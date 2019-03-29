34686130cf3d843eccb76763d527df15c9a61e52
wolfs25 <sebastian.wolf.sw1@roche.com>
2019-03-29 18:39:09 +0000

    added examples for chunks
    I tried to implement some of the examples that Adrian had for code chunks

--- dev/try_chunk_parser.R
+++ dev/try_chunk_parser.R
@@ -33,12 +33,8 @@ is_teal_data <- function(x){
 }
 
 get_variables_from_call <- function(input_call, envir = parent.frame()){
-  variables_to_define <- lapply(ls(envir),
-      locate_variables,
-      string = as.character(input_call)
-  )
-  variables_to_define <- Filter(f = length_check, variables_to_define)
-  variables_to_define <- map(variables_to_define, ~ unique(.))
+
+  variables_to_define <- ls(envir)
 
   variables_to_define %<>% map(~ get(., envir)) %>% setNames(variables_to_define)
 
@@ -57,15 +53,20 @@ get_variables_from_call <- function(input_call, envir = parent.frame()){
 
 #' @importFrom rlang call2
 #' @importFrom magrittr %>%
-pipe_chunk <- function()
+pipe_chunk_test <- function()
 {
 
-  function(lhs,rhs){
+  function(lhs, rhs){
     pipe <- as.character(match.call()[[1]]) %>% gsub(pattern="%", replacement = "")
 
     chunk_name <- match.call()[[2]] %>% as.character
     right_hand_side <- match.call()[[3]]
 
+    if(!is.null(global_chunks$code_chunks[[chunk_name]])){
+
+      chunk_existing <- global_chunks$code_chunks[[chunk_name]]$expression
+      right_hand_side <- paste(chunk_existing %>% deparse(), right_hand_side %>% deparse(width = 2000)) %>% parse_expr()
+    }
     # chunk with assignment
     if (pipe == "<chunk>"){
       p <- parent.frame()
@@ -77,32 +78,59 @@ pipe_chunk <- function()
     variable_list <- get_variables_from_call(right_hand_side, parent.frame())
 
     if("dataset" %in% names(variable_list) && "dataname" %in% names(variable_list)){
-      right_hand_side <- right_hand_side %>% deparse() %>%
+      right_hand_side <- right_hand_side %>% deparse() %>% paste(collapse = "") %>%
           gsub(pattern = variable_list$dataname, replacement = "dataset")  %>%
           parse_expr()
     }
-
     set_chunk(
         id = chunk_name,
         expr = right_hand_side,
-        vars = variable_list,
+        vars = parent.frame(),
         chunks = global_chunks
         )
 
   }
 }
-`%chunk>%` <- pipe_chunk()
+`%<chunk%` <- pipe_chunk_test()
 
-`%<chunk>%` <- pipe_chunk()
+`%<chunk>%` <- pipe_chunk_test()
 
 # Evaluation as user sees it:
 
-merged_dataset <- data.frame(x = c(1,2), y = c(2,2))
+a <- 3
 
+chunk1 <- quote(a + y + 5)
+chunk1 %<>% substituteDirect(list(y = 3))
+chunk1 %>% eval()
+
+
+
+# Merged dataset is a teal dataset and will by default never
+# be substituted
+merged_dataset <- data.frame(x = c(1,2), y = c(2,2))
 attr(merged_dataset, "keys") <- "x"
 
+a %<chunk>% data.frame(x = c(1,2), y = c(2,2))
+
+b %<chunk>% 13
+
+a1 %<chunk% ggplot(data = a, aes(x = x, y = y))
+a1 %<chunk% + theme_bw(base_size = b)
+a1 %<chunk% +geom_point()
+
+print(eval_chunk("a1", global_chunks))
+print(get_code_chunk(id = "a1", chunks = global_chunks))
+global_chunks$get_rcode(chunk_ids = c("a","b","a1")) %>%
+    paste(collapse = "\n") %>%
+    cat
+
+remove_chunk("b", global_chunks)
+
+global_chunks$
+
+stop("X")
 fit %<chunk>%
-  lm(as.formula("x ~ x"), data = merged_dataset)
+  lm(as.formula("x ~ x"), data = !!merged_dataset)
 
 #fit <- lm(as.formula("x ~ x"), data = merged_dataset)
 
@@ -117,7 +145,7 @@ i <- which(plot_type == c(
 if (plot_type == "Response vs Regressor") {
   if (ncol(fit$model) > 1) {
     validate(need(dim(fit$model)[2] < 3, "Response vs Regressor is not provided for >2 Regressors"))
-    plot %chunk>%
+    "plot" %<chunk%
         plot(fit$model[, 2:1])
   } else {
     plot %chunk>% {
