d3558216e703d61b27a0dd77277190f8d2f13380
Dawid Kałędkowski <dawid.kaledkowski@contractors.roche.com>
2019-04-08 12:53:59 +0200

    include tests

--- inst/app_source1.R
+++
@@ -1,5 +0,0 @@
-asl <- radsl(N = 600)
-adte <- radtte(asl, event.descr = c("STUDYID", "USUBJID", "PARAMCD"))
-
-keys(asl) <- c("USUBJID", "STUDYID")
-keys(adte) <- c("USUBJID", "STUDYID", "PARAMCD")
