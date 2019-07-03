79c4710718c53e0e91351f26672ec840f8fbb400
Dawid Kałędkowski <dawid.kaledkowski@contractors.roche.com>
2019-07-03 14:27:55 +0200

    tests for filter_specs
    closes #118 and #129

--- R/utils.R
+++ R/utils.R
@@ -11,3 +11,24 @@ split_by_sep <- function(x, sep) {
   stopifnot(is.atomic(x))
   strsplit(x, sep, fixed = TRUE)
 }
+
+#' List element in other list
+#'
+#' Checks if \code{x} element matches any of \code{y} element. If one of the arguments is a list then list elements
+#' are treated as whole - in this case list elements can be a vector, so it looks
+#' for equal element in second vector to be matched.
+#'
+#' @param x \code{list} to be matched.
+#' @param y \code{list} to be matched against.
+#' @return \code{logical} vector length of \code{x} denoting if element was found in second list.
+#' @export
+`%is_in%`  <- function(x, y) {
+  if (!is.list(x) & is.list(y)) {
+    x <- list(x)
+  } else if (!is.list(y) & is.list(x)) {
+    y <- list(y)
+  }
+  vapply(x, function(x1) {
+    any(vapply(y, function(y1) isTRUE(all.equal(y1, x1)), logical(1), USE.NAMES = FALSE))
+  }, logical(1), USE.NAMES = FALSE)
+}
---
+++ tests/testthat/test_filter_spec.R
@@ -0,0 +1,120 @@
+context("filter_spec")
+
+choices <- c("val1", "val2", "val3")
+choices_d <- c("val1", "val1", "val2", "val3")
+choices_f <- as.factor(choices)
+choices_l <- as.list(choices)
+
+test_that("Proper argument types", {
+  expect_error(filter_spec(vars = list("var"), choices = choices), "is.atomic")
+  expect_error(filter_spec(vars = "var", choices = choices_l), "is.atomic")
+  expect_error(filter_spec(vars = "var", choices = choices, selected = list("val2")), "is.atomic")
+  expect_error(filter_spec(vars = 1, choices = choices, selected = choices[1]), "is.character")
+  expect_error(filter_spec(vars = "var", choices = 1:3, selected = 1), "is.character")
+  expect_error(filter_spec(vars = factor("var"), choices = choices, selected = choices[1]), "is.character")
+  expect_error(filter_spec(vars = "var", choices = choices_f, selected = choices_f[1]), "is.character")
+  expect_error(filter_spec(vars = "var", choices = choices, multiple = 1), "is.logical")
+  expect_error(filter_spec(vars = "var", choices = choices, label = factor("test")), "is.character")
+
+  expect_error(filter_spec(vars = "var", choices = choices_d), "duplicated")
+  expect_error(filter_spec(vars = "var", choices = choices, selected = c("val1", "val1")), "duplicated")
+
+  expect_error(filter_spec(vars = "var", choices = choices, label = c("test", "test2")), "is.character.single")
+  expect_error(filter_spec(vars = "var", choices = choices, sep = c("-", ",")), "is.character.single")
+})
+
+
+test_that("Single choice", {
+  expect_silent(f1 <- filter_spec(vars = "var1",
+                                  choices = choices,
+                                  selected = choices[1],
+                                  multiple = FALSE,
+                                  label = "test"))
+  expect_identical(names(f1), c("vars", "choices", "selected", "multiple", "fixed", "label"))
+  expect_identical(f1$choices, as.list(setNames(choices, choices)))
+  expect_identical(f1$selected, as.list(setNames(choices[1], choices[1])))
+
+  expect_false(f1$multiple)
+  expect_true(f1$fixed)
+  expect_identical(f1$label, "test")
+})
+
+test_that("Multiple choices", {
+  expect_error(filter_spec(vars = "var1", choices = choices, selected = choices[1:2], multiple = FALSE), "multiple")
+  expect_silent(f1 <- filter_spec(vars = "var1", choices = choices, selected = choices[1:2], multiple = TRUE))
+  expect_silent(f2 <- filter_spec(vars = "var1", choices = choices, selected = choices[1:2]))
+  expect_identical(f1, f2)
+
+  expect_true(f1$multiple)
+  expect_identical(f1$choices, list(val1 = "val1", val2 = "val2", val3 = "val3"))
+  expect_identical(f1$selected, list(val1 = "val1", val2 = "val2"))
+})
+
+test_that("Multiple vars", {
+  expect_error(filter_spec(vars = c("var1", "var2"), choices = c("val1.1-val2.1", "val1.1-val2.2"), sep = ":"),
+               "length\\(vars\\)")
+
+  expect_error(filter_spec(vars = c("var1", "var2"), choices = c("val1.1-val2.1", "val1.1-val2.2")),
+               "length\\(vars\\)")
+
+  expect_error(filter_spec(vars = "var1", choices = c("val-1", "val2", "val3", "val4"), sep = "-"),
+               "length\\(vars\\)")
+
+  expect_silent(f1 <- filter_spec(vars = c("var1", "var2"),
+                                  choices = c("val1.1 - val2.1", "val1.1 - val2.2"),
+                                  sep = " - "))
+
+  expect_silent(f2 <- filter_spec(vars = c("var1", "var2"),
+                                  choices = c("val1.1 - val2.1", "val1.1 - val2.2")))
+
+  expect_silent(f3 <- filter_spec(vars = c("var1", "var2"),
+                                  choices = c(`val1.1 - val2.1` = "val1.1 - val2.1",
+                                              `val1.1 - val2.2` = "val1.1 - val2.2")))
+
+  expect_silent(f4 <- filter_spec(vars = c("var1", "var2"),
+                                  choices = c(`combo1` = "val1.1 - val2.1",
+                                              `combo2` = "val1.1 - val2.2")))
+
+  expect_identical(f1, f2)
+  expect_identical(f1, f3)
+  expect_true(all(names(f1$choices) != names(f4$choices)))
+
+  expect_identical(f1$vars, c("var1", "var2"))
+  expect_identical(names(f1$choices), c("val1.1 - val2.1", "val1.1 - val2.2"))
+  expect_identical(names(f4$choices), c("combo1", "combo2"))
+  expect_identical(f1$selected, list(`val1.1 - val2.1` = c("val1.1", "val2.1")))
+
+  # Multiple vars and multiple = TRUE
+  choices <- c("val1.1 - val2.1", "val1.1 - val2.2", "val1.1 - val2.3")
+
+  expect_silent(
+    f1m <- filter_spec(vars = c("var1", "var2"),
+                      choices = choices,
+                      selected = choices[1:2],
+                      multiple = TRUE,
+                      sep = " - ")
+  )
+
+  expect_silent(
+    f2m <- filter_spec(vars = c("var1", "var2"),
+                       choices = choices,
+                       selected = choices[1:2],
+                       sep = " - ")
+  )
+
+  expect_identical(f1m, f2m)
+
+  # correct obbject structure
+  expect_identical(names(f1m), c("vars", "choices", "selected", "multiple", "fixed", "label"))
+  expect_identical(f1m$choices, list(`val1.1 - val2.1` = c("val1.1", "val2.1"),
+                                     `val1.1 - val2.2` = c("val1.1", "val2.2"),
+                                     `val1.1 - val2.3` = c("val1.1", "val2.3")))
+  expect_identical(f1m$selected, list(`val1.1 - val2.1` = c("val1.1", "val2.1"),
+                                      `val1.1 - val2.2` = c("val1.1", "val2.2")))
+
+  expect_true(f1m$multiple)
+  expect_true(f1m$fixed)
+  expect_identical(f1m$label, "Filter")
+
+
+})
