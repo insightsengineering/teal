7e2761ddd26b62ab9db338d9147167b27ae55272
Junlue Zhao <jzhao31415@gmail.com>
2021-08-06 07:32:04 -0500

    test file path

--- tests/testthat/test-mutate_dataset.R
+++ tests/testthat/test-mutate_dataset.R
@@ -78,10 +78,8 @@ test_that("mutate_dataset", {
                stringsAsFactors = FALSE)
   )
 
-  test_script <- system.file("tests", "testthat", "mutate_code", "testds.R", package = "teal")
-
   expect_silent({
-    test_ds_mut <- test_ds %>% mutate_dataset(read_script(test_script))
+    test_ds_mut <- test_ds %>% mutate_dataset(read_script("mutate_code/testds.R"))
   })
 
   expect_equal(
@@ -99,7 +97,7 @@ test_that("mutate_dataset", {
   expect_true(is(test_ds_mut, "Dataset"))
 
   expect_silent({
-    test_ds_mut <- test_ds %>% mutate_dataset(script = test_script)
+    test_ds_mut <- test_ds %>% mutate_dataset(script = read_script("mutate_code/testds.R"))
   })
 
   expect_equal(
