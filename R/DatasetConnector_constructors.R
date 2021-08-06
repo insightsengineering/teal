10a777909b5c6d6c16af22685ea7dd2c3babce39
Konrad Pagacz <konrad.pagacz@contractors.roche.com>
2021-08-06 09:21:05 +0200

    reinistated check_pkg_

--- R/DatasetConnector_constructors.R
+++ R/DatasetConnector_constructors.R
@@ -722,6 +722,12 @@ rice_dataset_connector <- function(dataname,
   dot_args <- list(...)
   stopifnot(is_fully_named_list(dot_args))
   stopifnot(is_character_single(path))
+  check_pkg_quietly(
+    "rice",
+    paste0(
+      "Connection to entimICE via rice was requested, but rice package is not available.",
+      "Please install it from https://github.roche.com/Rpackages/rice.")
+  )
 
   x_fun <- callable_function("rice::rice_read") # nolint
   args <- append(list(node = path, prolong = TRUE), dot_args)
