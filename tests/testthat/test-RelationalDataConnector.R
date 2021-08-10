a915c5e197bdc2f9ee6de7d8750f0e0ccd603d59
Konrad Pagacz <konrad.pagacz@contractors.roche.com>
2021-08-10 14:43:17 +0200

    Renamed test files to match tested objects with their class names

--- tests/testthat/test-RelationalDataConnector.R
+++ tests/testthat/test-RelationalDataConnector.R
@@ -1,49 +1,5 @@
 library(scda)
 
-test_that("data connection", {
-  open_fun <- callable_function(data.frame)
-  open_fun$set_args(list(x = 1:5))
-
-  close_fun <- callable_function(data.frame)
-  close_fun$set_args(list(x = 1:2))
-
-  con <- DataConnection$new(open_fun = open_fun, close_fun = close_fun)
-  con$set_open_args(args = list(y = letters[1:5]))
-
-  expect_identical(
-    as.list(con$get_open_call(deparse = FALSE)),
-    list(as.name("data.frame"), x = 1:5, y = letters[1:5])
-  )
-
-  expect_identical(
-    con$get_open_call(),
-    "data.frame(x = 1:5, y = c(\"a\", \"b\", \"c\", \"d\", \"e\"))"
-  )
-
-
-  expect_false(con$.__enclos_env__$private$opened)
-  con$open()
-  expect_true(con$.__enclos_env__$private$opened)
-
-  # passing arguments to open doesn't overwrite args
-  con$open(args = list(x = 1:5, y = LETTERS[1:5]))
-  expect_identical(
-    as.list(con$get_open_call(deparse = FALSE)),
-    list(as.name("data.frame"), x = 1:5, y = letters[1:5])
-  )
-
-
-  expect_identical(
-    con$get_open_call(),
-    "data.frame(x = 1:5, y = c(\"a\", \"b\", \"c\", \"d\", \"e\"))"
-  )
-
-  expect_null(
-    con$close(silent = TRUE)
-  )
-
-})
-
 test_that("RelationalDataConnector with DataConnection", {
   open_fun <- callable_function(data.frame)
   open_fun$set_args(list(x = 1:5))
