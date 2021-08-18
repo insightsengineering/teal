4dbd8a36a0a8900b8774d2cbd9f4a637a7740c8c
Junlue Zhao <jzhao31415@gmail.com>
2021-08-18 12:28:01 -0500

    added back original pawel bug to tests

--- tests/testthat/test-CDISCDataset.R
+++ tests/testthat/test-CDISCDataset.R
@@ -29,7 +29,7 @@ testthat::test_that("CDISCDataset basics", {
   )
 })
 
-testthat::test_that("CDISCDataset$get_code() does not return duplicated code when
+testthat::test_that("Case 1: CDISCDataset$get_code() does not return duplicated code when
   CDISCDataset$mutate method is called", {
   iris_dataset <- CDISCDataset$new("iris", head(iris), code = "head(iris)", parent = character(0), keys = c("test"))
   mtcars_dataset <- CDISCDatasetConnector$new(
@@ -54,3 +54,32 @@ testthat::test_that("CDISCDataset$get_code() does not return duplicated code whe
     )
   )
 })
+
+testthat::test_that("Case 2: CDISCDataset$get_code() does not return duplicated code when
+  CDISCDataset$mutate method is called", {
+    adsl_d <- cdisc_dataset("ADSL", head(iris))
+    adsl_d %>% mutate_dataset("ADSL$a <- x", vars = list(x = 1)) %>% mutate_dataset("ADSL$b <- y", vars = list(y = 2))
+
+    adae_d <- rcd_cdisc_dataset_connector("ADAE", function() head(mtcars))
+    adae_d %>% mutate_dataset("ADAE$a <- x", vars = list(x = 1))
+    adae_d %>% mutate_dataset("ADAE$a <- ADAE$a*2")
+    adae_d %>% load_dataset() %>% mutate_dataset("ADAE$a <- ADAE$a*2")
+
+    adsl_d %>% mutate_dataset("ADSL$c <- z", vars = list(z = 3))
+    adsl_d %>% mutate_dataset("ADSL$d <- ADAE$a[[1]]", vars = list(ADAE = adae_d))
+    testthat::expect_equal(
+      adsl_d %>% get_code() %>% pretty_code_string(),
+      c("x <- 1",
+        "y <- 2",
+        "z <- 3",
+        "ADAE <- (function() head(mtcars))()",
+        "ADAE$a <- x",
+        "ADAE$a <- ADAE$a * 2",
+        "ADAE$a <- ADAE$a * 2",
+        "ADSL$a <- x",
+        "ADSL$b <- y",
+        "ADSL$c <- z",
+        "ADSL$d <- ADAE$a[[1]]"
+      )
+    )
+  })
