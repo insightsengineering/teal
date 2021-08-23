849cebd4525f2d35deed564411790c8075151948
Konrad Pagacz <konrad.pagacz@contractors.roche.com>
2021-08-23 12:16:43 +0200

    Test fixes

--- tests/testthat/test-RelationalData.R
+++ tests/testthat/test-RelationalData.R
@@ -165,10 +165,10 @@ testthat::test_that("Multiple rcd_data connectors wrapped in cdisc_data", {
 
   testthat::expect_equal(unname(get_dataname(data)), c("ADSL", "ADAE", "ADVS", "ADTTE"))
 
-  testthat::expect_equal(items$ADSL$get_code(), "radsl(N = 1)")
-  testthat::expect_equal(items$ADAE$get_code(), "radae(ADSL = ADSL)")
-  testthat::expect_equal(items$ADVS$get_code(), "radvs(ADSL = ADSL)")
-  testthat::expect_equal(items$ADTTE$get_code(), "radtte(ADSL = ADSL)")
+  testthat::expect_equal(items$ADSL$get_code(), "ADSL <- radsl(N = 1)")
+  testthat::expect_equal(items$ADAE$get_code(), "ADSL <- radsl(N = 1)\nADAE <- radae(ADSL = ADSL)")
+  testthat::expect_equal(items$ADVS$get_code(), "ADSL <- radsl(N = 1)\nADVS <- radvs(ADSL = ADSL)")
+  testthat::expect_equal(items$ADTTE$get_code(), "ADSL <- radsl(N = 1)\nADTTE <- radtte(ADSL = ADSL)")
 
   testthat::expect_equal(
     get_code(data, "ADSL"),
