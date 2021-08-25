9f406c810a2117a9f949b9f323bef360d83ad93e
Konrad Pagacz <konrad.pagacz@contractors.roche.com>
2021-08-25 10:38:09 +0200

    mae tests wip

--- tests/testthat/test-MAEFilterStates.R
+++ tests/testthat/test-MAEFilterStates.R
@@ -35,9 +35,12 @@ testthat::test_that("get_call returns a call filtering an MAE object", {
   data(miniACC)
   filter_states <- MAEFilterStates$new(
     input_dataname = "test",
-    output_dataname = "test",
+    output_dataname = "output",
     datalabel = character(0),
     varlabels = character(0),
     keys = character(0)
   )
+  filter_state <- ChoicesFilterState$new(c("white"), varname = "race", use_dataname = TRUE)
+  filter_states$queue_push(x = filter_state, queue_index = 1, element_id = "test")
+  isolate(filter_states$get_call())
 })
