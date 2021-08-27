48ea58c15b0e6024a2296c1bf77e9a040ba089e9
Junlue Zhao <jzhao31415@gmail.com>
2021-08-27 07:43:29 -0500

    fixed unit tests after merge

--- tests/testthat/test-CDISCFilteredDataset.R
+++ tests/testthat/test-CDISCFilteredDataset.R
@@ -5,8 +5,7 @@ testthat::test_that("The constructor accepts a CDISCDataset and an empty list", 
       head(iris),
       parent = character(0),
       keys = c("Petal.Length")
-    ),
-    join_keys = list()
+    )
   ), NA)
 })
 
@@ -17,8 +16,7 @@ testthat::test_that("get_call returns a list of calls", {
       head(iris),
       parent = character(0),
       keys = c("Petal.Length")
-    ),
-    join_keys = list()
+    )
   )
   testthat::expect_true(is_class_list("language")(filtered_dataset$get_call()))
 })
@@ -30,8 +28,20 @@ testthat::test_that("get_call returns a list call for a CDISCDataset with a pare
       head(iris),
       parent = "test",
       keys = c("Petal.Length")
-    ),
-    join_keys = list()
+    )
   )
   testthat::expect_true(is_class_list("language")(filtered_dataset$get_call()))
+})
+
+testthat::test_that("get_subjects_info(FALSE) returns number of unique
+  observations in the columns designated by the keys argument", {
+  filtered_dataset <- CDISCFilteredDataset$new(
+    dataset = CDISCDataset$new(
+      "iris",
+      head(iris),
+      parent = character(0),
+      keys = c("Petal.Length")
+    )
+  )
+  testthat::expect_equal(filtered_dataset$get_subjects_info(FALSE), length(unique(head(iris)$Petal.Length)))
 })
