9dfdd17c587209ba5cc989a8722acb2a7fa5a079
mhallal1 <86970066+mhallal1@users.noreply.github.com>
2021-08-27 06:29:53 +0200

    Update R/FilteredData.R
    Co-authored-by: Pawel Rucki <pawel.rucki@roche.com>

--- R/FilteredData.R
+++ R/FilteredData.R
@@ -677,12 +677,13 @@ FilteredData <- R6::R6Class( # nolint
         datasets_df <- self$get_filter_overview(datanames = datanames)
 
         datasets_html <- lapply(seq_len(nrow(datasets_df)), function(x) {
-              tags$tr(
-                tags$td(rownames(datasets_df)[x]),
-                tags$td(datasets_df[x, 1]),
-                tags$td(datasets_df[x, 2]))
-            }
-        )
+          tags$tr(
+            tags$td(rownames(datasets_df)[x]),
+            tags$td(datasets_df[x, 1]),
+            tags$td(datasets_df[x, 2])
+          )
+        }
+      )
 
         table_html <- tags$table(
           class = "table custom-table",
