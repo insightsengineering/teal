8b448be02e03eb15e1c80c621f97128112d98d48
Junlue Zhao <jzhao31415@gmail.com>
2021-08-31 12:29:06 -0500

    added tests

--- tests/testthat/test-SEFilterStates.R
+++ tests/testthat/test-SEFilterStates.R
@@ -14,3 +14,26 @@ testthat::test_that("The constructor initializes two queues", {
   )
   testthat::expect_null(filter_states$queue_get(2))
 })
+
+
+testthat::test_that("set_bookmark_state throws error on bad inputs", {
+  filter_states <- SEFilterStates$new(
+    input_dataname = "test",
+    output_dataname = "test",
+    datalabel = "test"
+  )
+  testthat::expect_error(filter_states$set_bookmark_state())
+  testthat::expect_error(filter_states$set_bookmark_state(data.frame(a = "test")))
+  testthat::expect_error(filter_states$set_bookmark_state(list(subset = "test", select = "test")))
+})
+
+testthat::test_that("set_bookmark_state returns NULL on good inputs", {
+  filter_states <- SEFilterStates$new(
+    input_dataname = "test",
+    output_dataname = "test",
+    datalabel = "test"
+  )
+  data <- data.frame(a = "test")
+  class(data) <- "SummarizedExperiment"
+  testthat::expect_null(filter_states$set_bookmark_state(data = data, state = list(subset = NULL, select = NULL)))
+})
