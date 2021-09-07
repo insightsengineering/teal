c62376fb1348be66767aa835d5ba08da11d2fdb7
Junlue Zhao <jzhao31415@gmail.com>
2021-09-07 08:58:20 -0500

    indentation

--- tests/testthat/test-SEFilterStates.R
+++ tests/testthat/test-SEFilterStates.R
@@ -3,12 +3,14 @@ get_test_data <- function() {
   nrows <- 200
   ncols <- 6
   counts <- matrix(runif(nrows * ncols, 1, 1e4), nrows)
-  row_ranges <- GRanges(rep(c("chr1", "chr2"), c(50, 150)),
-                        IRanges(floor(runif(200, 1e5, 1e6)), width = 100),
-                        strand = sample(c("+", "-"), 200, TRUE),
-                        feature_id = sprintf("ID%03d", 1:200))
-  cdata <- DataFrame(Treatment = rep(c("ChIP", "Input"), 3),
-                     row.names = LETTERS[1:6])
+  row_ranges <- GRanges(
+    rep(c("chr1", "chr2"), c(50, 150)),
+    IRanges(floor(runif(200, 1e5, 1e6)), width = 100),
+    strand = sample(c("+", "-"), 200, TRUE),
+    feature_id = sprintf("ID%03d", 1:200))
+  cdata <- DataFrame(
+    Treatment = rep(c("ChIP", "Input"), 3),
+    row.names = LETTERS[1:6])
 
   obj <- SummarizedExperiment(
     assays = list(counts = counts),
