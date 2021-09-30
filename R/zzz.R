6cdfaa2f83f8dc8411998da8de922ba9e1241216
kpagacz <konrad.pagacz@contractors.roche.com>
2021-09-30 15:29:32 +0200

    fix(MAE crash): reassigned the fixed S4Vectors function Closes #210

--- R/zzz.R
+++ R/zzz.R
@@ -17,6 +17,25 @@
     utils.nest::get_package_file("teal", "cdisc_datasets/cdisc_datasets.yaml")) #nolint
   assign("default_cdisc_keys", default_cdisc_keys, envir = parent.env(environment()))
 
+  # Fixes https://github.com/insightsengineering/teal/issues/210
+  # R versions < 4.1 don't have the access to the updated version of S4Vectors
+  # hence we need to copy the fix explicitly
+  normSubsetIndex <- function(i) { # nolint
+    i <- try(as.logical(i), silent = TRUE)
+    if (inherits(i, "try-error"))
+      stop("'subset' must be coercible to logical")
+    i & !is.na(i)
+  }
+  evalqForSubset <- function(expr, envir, ...) { # nolint
+    if (methods::missingArg(substitute(expr), parent.frame())) {
+      rep(TRUE, NROW(envir))
+    } else {
+      i <- evalArg(substitute(expr), envir, ..., where = parent.frame())
+      normSubsetIndex(i)
+    }
+  }
+  R.utils::reassignInPackage(name = "evalqForSubset", pkgname = "S4Vectors", value = evalqForSubset())
+
   return(invisible())
 }
 
