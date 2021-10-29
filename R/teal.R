dea7f9e324db34cc6d98e11e62139c156b03b77c
kpagacz <konrad.pagacz@contractors.roche.com>
2021-10-29 11:12:35 +0200

    fix test.nest errors

--- R/teal.R
+++ R/teal.R
@@ -25,3 +25,12 @@ NULL
 #' @importFrom MultiAssayExperiment MultiAssayExperiment
 #' @importFrom SummarizedExperiment colData
 NULL
+
+# This is a dummy function, which only purpose is to call
+# `lifecycle::badge`. `test.nest` gives a false positive error
+# when testing usage of an imported function used only in the
+# Roxygen docstring context. To remove the error, teal needs
+# to call this function somewhere else - so here it is.
+dummy_function_for_test_nest <- function() { # nousage
+  lifecycle::badge("experimental")
+}
