2f4598d9daef208c1068876661792bb4c49af2b7
Maciej Nasinski <nasinski.maciej@gmail.com>
2021-11-04 10:09:30 +0100

    Update inst/js/sidebar.js
    Co-authored-by: Pawel Rucki <pawel.rucki@roche.com>

--- inst/js/sidebar.js
+++ inst/js/sidebar.js
@@ -3,7 +3,7 @@ function toggle_sidebar() {
   if ($("#teal_secondary_col").css("display") == "none") {
     $("#teal_primary_col").attr("class", "col-sm-9").resize();
     $("#teal_secondary_col").delay(600).fadeIn(50);
-     filter_open = true;
+    filter_open = true;
   } else {
     $("#teal_secondary_col").fadeOut(1);
     $("#teal_primary_col").attr("class", "col-sm-12").resize();
