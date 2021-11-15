ec99959db17d0ffe2656fcc880b08f02d73d022e
Mahmoud Hallal <mahmoud.hallal@roche.com>
2021-11-15 19:14:32 +0100

    update tests

--- tests/testthat/test-MAEFilteredDataset.R
+++ tests/testthat/test-MAEFilteredDataset.R
@@ -1,32 +1,32 @@
-library(MultiAssayExperiment)
-data(miniACC)
-
 testthat::test_that("MAEFilteredDataset accepts a MAEDataset object", {
-  testthat::expect_error(MAEFilteredDataset$new(dataset = MAEDataset$new("miniACC", miniACC)), NA)
+  testthat::expect_error(MAEFilteredDataset$new(dataset = MAEDataset$new("miniACC", MultiAssayExperiment::miniACC)), NA)
 })
 
-testthat::test_that("MAEFilteredDataset throws error with non-MultiAssayExperiment dataset", {
+testthat::test_that("MAEFilteredDataset throws error with non-MAEDataset dataset", {
   testthat::expect_error(
-    MAEFilteredDataset$new(dataset = MAEDataset$new("iris", head(iris))),
-    "is(x, \"MultiAssayExperiment\") is not TRUE"
+    MAEFilteredDataset$new(dataset = Dataset$new("iris", head(iris))),
+    "is(dataset, \"MAEDataset\") is not TRUE"
     , fixed = TRUE)
 })
 
 testthat::test_that("MAEFilteredDataset$get_call returns a call without applying filter", {
-  filtered_dataset <- MAEFilteredDataset$new(dataset = MAEDataset$new("miniACC", miniACC))
+  filtered_dataset <- MAEFilteredDataset$new(dataset = MAEDataset$new("miniACC", MultiAssayExperiment::miniACC))
   get_call_output <- filtered_dataset$get_call()
   testthat::expect_true(is_class_list("language")(get_call_output))
   testthat::expect_identical(deparse(get_call_output$subjects), "miniACC_FILTERED <- miniACC")
 })
 
 testthat::test_that("MAEFilteredDataset$get_call returns a call with applying filter", {
-  filtered_dataset <- MAEFilteredDataset$new(dataset = MAEDataset$new("miniACC", miniACC))
+  filtered_dataset <- MAEFilteredDataset$new(dataset = MAEDataset$new("miniACC", MultiAssayExperiment::miniACC))
   filter_state_mae <- ChoicesFilterState$new(
-    x = c("white"),
+    x = MultiAssayExperiment::miniACC$race,
     varname = as.name("race"),
     input_dataname = as.name("miniACC"),
     extract_type = "list"
   )
+
+  filter_state_mae$set_selected("white")
+  filter_state_mae$set_na_rm(TRUE)
 
   queue <- filtered_dataset$get_filter_states(1)
   queue$queue_push(filter_state_mae, queue_index = 1L, element_id = "race")
@@ -35,32 +35,40 @@ testthat::test_that("MAEFilteredDataset$get_call returns a call with applying fi
 
   testthat::expect_true(is_class_list("language")(get_call_output))
   testthat::expect_identical(
-    deparse(get_call_output$subjects),
-    c("miniACC_FILTERED <- MultiAssayExperiment::subsetByColData(miniACC, ", "    y = miniACC$race == \"white\")")
+    get_call_output$subjects,
+    quote(
+      miniACC_FILTERED <- MultiAssayExperiment::subsetByColData(
+        miniACC,
+        y = !is.na(miniACC$race) & miniACC$race == "white"
+      )
+    )
   )
 })
 
 testthat::test_that("MAEFilteredDataset$get_data throws error without filtered argument given", {
-  filtered_dataset <- MAEFilteredDataset$new(dataset = MAEDataset$new("miniACC", miniACC))
+  filtered_dataset <- MAEFilteredDataset$new(dataset = MAEDataset$new("miniACC", MultiAssayExperiment::miniACC))
   expect_error(isolate(filtered_dataset$get_data()), "argument \"filtered\" is missing, with no default")
 })
 
 testthat::test_that("MAEFilteredDataset$get_data returns identical filtered and
                     non-filtered MAE data when no filter is applied", {
-  filtered_dataset <- MAEFilteredDataset$new(dataset = MAEDataset$new("miniACC", miniACC))
+  filtered_dataset <- MAEFilteredDataset$new(dataset = MAEDataset$new("miniACC", MultiAssayExperiment::miniACC))
   filtered_mae <- isolate(filtered_dataset$get_data(filtered = TRUE))
   non_filtered_mae <- isolate(filtered_dataset$get_data(filtered = FALSE))
   expect_identical(filtered_mae, non_filtered_mae)
 })
 
 testthat::test_that("MAEFilteredDataset get_data returns filtered MAE data when filter is applied", {
-  filtered_dataset <- MAEFilteredDataset$new(dataset = MAEDataset$new("miniACC", miniACC))
+  filtered_dataset <- MAEFilteredDataset$new(dataset = MAEDataset$new("miniACC", MultiAssayExperiment::miniACC))
   filter_state_mae <- ChoicesFilterState$new(
-    x = c("white"),
+    x = MultiAssayExperiment::miniACC$race,
     varname = as.name("race"),
     input_dataname = as.name("miniACC"),
     extract_type = "list"
   )
+
+  filter_state_mae$set_selected("white")
+  filter_state_mae$set_na_rm(TRUE)
 
   queue <- filtered_dataset$get_filter_states(1)
   queue$queue_push(filter_state_mae, queue_index = 1L, element_id = "race")
@@ -68,12 +76,12 @@ testthat::test_that("MAEFilteredDataset get_data returns filtered MAE data when 
   filtered_mae <- isolate(filtered_dataset$get_data(filtered = TRUE))
   non_filtered_mae <- isolate(filtered_dataset$get_data(filtered = FALSE))
 
-  expect_false(identical(filtered_mae, non_filtered_mae))
-  expect_identical(unique(filtered_mae$race), "white")
+  testthat::expect_false(identical(filtered_mae, non_filtered_mae))
+  testthat::expect_identical(unique(filtered_mae$race), "white")
 })
 
 testthat::test_that("get_filter_overview_info returns overview matrix for MAEFilteredDataset without filtering", {
-  filtered_dataset <- MAEFilteredDataset$new(dataset = MAEDataset$new("miniACC", miniACC))
+  filtered_dataset <- MAEFilteredDataset$new(dataset = MAEDataset$new("miniACC", MultiAssayExperiment::miniACC))
   testthat::expect_equal(
     isolate(filtered_dataset$get_filter_overview_info()),
     matrix(
@@ -89,7 +97,7 @@ testthat::test_that("get_filter_overview_info returns overview matrix for MAEFil
 })
 
 testthat::test_that("get_filter_overview_info returns overview matrix for MAEFilteredDataset with filtering", {
-  filtered_dataset <- MAEFilteredDataset$new(dataset = MAEDataset$new("miniACC", miniACC))
+  filtered_dataset <- MAEFilteredDataset$new(dataset = MAEDataset$new("miniACC", MultiAssayExperiment::miniACC))
 
   filter_state_mae <- ChoicesFilterState$new(
     x = c("white", NA_character_),
@@ -152,7 +160,7 @@ testthat::test_that("MAEFilteredDataset$set_bookmark_state sets filters in Filte
 testthat::test_that("MAEFilteredDataset$set_bookmark_state throws error if state argument is not a list ", {
   dataset <- teal:::MAEFilteredDataset$new(dataset("MAE", MultiAssayExperiment::miniACC))
   fs <- c("not_list")
-  expect_error(
+  testthat::expect_error(
     shiny::testServer(dataset$set_bookmark_state, args = list(state = fs), expr = NULL),
     "is.list(state) is not TRUE",
     fixed = TRUE
@@ -160,25 +168,59 @@ testthat::test_that("MAEFilteredDataset$set_bookmark_state throws error if state
 })
 
 testthat::test_that("MAEFilteredDataset$get_filterable_varnames returns character(0)", {
-  filtered_dataset <- MAEFilteredDataset$new(dataset = MAEDataset$new("miniACC", miniACC))
-  expect_identical(filtered_dataset$get_filterable_varnames(), character(0))
+  filtered_dataset <- MAEFilteredDataset$new(dataset = MAEDataset$new("miniACC", MultiAssayExperiment::miniACC))
+  testthat::expect_identical(filtered_dataset$get_filterable_varnames(), character(0))
 })
 
-testthat::test_that("MAEFilteredDataset$srv_add_filter_state adds filters", {
-  filtered_dataset <- MAEFilteredDataset$new(dataset = MAEDataset$new("miniACC", miniACC))
-  filter_state_mae <- ChoicesFilterState$new(
-    x = c("white"),
-    varname = as.name("race"),
-    input_dataname = as.name("miniACC"),
-    extract_type = "list"
+testthat::test_that("MAEFilteredDataset filters removed using remove_filters", {
+  filtered_dataset <- MAEFilteredDataset$new(dataset("MAE", MultiAssayExperiment::miniACC))
+  fs <- list(
+    subjects = list(
+      years_to_birth = c(30, 50),
+      vital_status = 1,
+      gender = "female"
+    ),
+    RPPAArray = list(
+      subset = list(ARRAY_TYPE = "")
+    )
   )
 
-  queue <- filtered_dataset$get_filter_states(1)
-  queue$queue_push(filter_state_mae, queue_index = 1L, element_id = "race")
-
-  shiny::testServer(filtered_dataset$srv_add_filter_state, expr = NULL)
-  expect_identical(
-    deparse(isolate(filtered_dataset$get_call())$subjects),
-    c("miniACC_FILTERED <- MultiAssayExperiment::subsetByColData(miniACC, ", "    y = miniACC$race == \"white\")")
+  shiny::testServer(
+    filtered_dataset$set_bookmark_state,
+    args = list(state = fs),
+    expr =  {
+      session$setInputs(remove_filters = FALSE)
+      testthat::expect_false(input$remove_filters)
+    }
   )
+
+  testthat::expect_identical(
+    isolate(filtered_dataset$get_call()),
+    list(
+      subjects = quote(
+        MAE_FILTERED <- MultiAssayExperiment::subsetByColData( # nolint
+          MAE,
+          y = MAE$years_to_birth >=  30 & MAE$years_to_birth <= 50 &
+            MAE$vital_status == "1" &
+            MAE$gender == "female"
+        )
+      ),
+      RPPAArray = quote(
+        MAE_FILTERED[["RPPAArray"]] <- subset( # nolint
+          MAE_FILTERED[["RPPAArray"]],
+          subset = ARRAY_TYPE == ""
+        )
+      )
+    )
+  )
+
+  shiny::testServer(
+    filtered_dataset$server,
+    expr = {
+      session$setInputs(remove_filters = TRUE)
+      testthat::expect_true(input$remove_filters)
+    }
+  )
+
+  testthat::expect_identical(isolate(filtered_dataset$get_call()), list(subjects = quote(MAE_FILTERED <- MAE)))
 })
