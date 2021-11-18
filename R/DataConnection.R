04a46e326109ebead3a2ff5df88aa4437ed02e61
Dawid Kałędkowski <6959016+gogonzo@users.noreply.github.com>
2021-11-18 15:34:06 +0100

    Update R/DataConnection.R
    Co-authored-by: Pawel Rucki <pawel.rucki@roche.com>

--- R/DataConnection.R
+++ R/DataConnection.R
@@ -660,8 +660,9 @@ DataConnection <- R6::R6Class( # nolint
     ping = function() {
       logger::log_trace("DataConnection$ping pinging the connection...")
       if (!is.null(private$ping_fun)) {
-        ping_res <- private$ping_fun$run()
-        return(isTRUE(ping_res))
+        ping_res <- isTRUE(private$ping_fun$run())
+        logger::log_trace("DataConnection$ping ping result: { res }.")
+        return(ping_res)
       } else {
         return(invisible(NULL))
       }
