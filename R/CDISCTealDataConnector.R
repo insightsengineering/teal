0ea8159fc19b566d9be3729daa74d262cb3f65bf
Pawel Rucki <pawelrucki@gmail.com>
2021-11-23 17:57:02 +0100

    pkgdown; too wide lines in examples

--- R/CDISCTealDataConnector.R
+++ R/CDISCTealDataConnector.R
@@ -67,10 +67,18 @@ CDISCTealDataConnector <- R6::R6Class( #nolint
 #'
 #' @examples
 #' \dontrun{
-#' adsl_cf <- CallableFunction$new(function() as.data.frame(as.list(setNames(nm = get_cdisc_keys("ADSL")))))
-#' adae_cf <- CallableFunction$new(function() as.data.frame(as.list(setNames(nm = get_cdisc_keys("ADAE")))))
-#' adsl <- CDISCTealDatasetConnector$new("ADSL", adsl_cf, keys = get_cdisc_keys("ADSL"), parent = character(0))
-#' adae <- CDISCTealDatasetConnector$new("ADAE", adae_cf, keys = get_cdisc_keys("ADAE"), parent = "ADSL")
+#' adsl_cf <- CallableFunction$new(
+#'   function() as.data.frame(as.list(setNames(nm = get_cdisc_keys("ADSL"))))
+#' )
+#' adae_cf <- CallableFunction$new(
+#'   function() as.data.frame(as.list(setNames(nm = get_cdisc_keys("ADAE"))))
+#' )
+#' adsl <- CDISCTealDatasetConnector$new(
+#'   "ADSL", adsl_cf, keys = get_cdisc_keys("ADSL"), parent = character(0)
+#' )
+#' adae <- CDISCTealDatasetConnector$new(
+#'   "ADAE", adae_cf, keys = get_cdisc_keys("ADAE"), parent = "ADSL"
+#' )
 #' data <- cdisc_data_connector(
 #'   connection = data_connection(open_fun = CallableFunction$new(function() "open function")),
 #'   connectors = list(adsl, adae)
