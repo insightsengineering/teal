6f085800d010484ae15b066a9c0c0c3c016a336c
Pawel Rucki <pawelrucki@gmail.com>
2021-11-29 17:02:55 +0100

    fix reference

--- R/data_extract_spec.R
+++ R/data_extract_spec.R
@@ -13,8 +13,8 @@
 #'
 #' @section Module Development:
 #' \describe{
-#' From this function's output a [teal.devel::data_extract_ui()] can
-#' be constructed. This input can be read by a [teal.devel::data_extract_module()] module.
+#' From this function's output a [teal.devel::data_extract_ui()] can be constructed.
+#' This input can be read by a [teal.devel::data_extract_srv()] module.
 #' }
 #'
 #' @param dataname (`character`) The name of the dataset to
