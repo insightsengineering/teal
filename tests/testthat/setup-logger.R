e39116ef68bed85f94da4c9da87747c73f18ed3f
Nikolas Burkoff <nikolas.burkoff@roche.com>
2021-12-01 14:16:29 +0000

    Update tests/testthat/setup-logger.R
    Co-authored-by: Konrad Pagacz <konrad.pagacz@contractors.roche.com>

--- tests/testthat/setup-logger.R
+++ tests/testthat/setup-logger.R
@@ -4,6 +4,7 @@
 # test file, call this function at the start of the file
 suppress_logs <- function() {
   old_log_appenders <- lapply(logger::log_namespaces(), function(ns) logger::log_appender(namespace = ns))
+  old_log_namespaces <- logger::log_namespaces()
   logger::log_appender(logger::appender_file(nullfile()), namespace = logger::log_namespaces())
-  withr::defer_parent(mapply(logger::log_appender, old_log_appenders, logger::log_namespaces()))
+  withr::defer_parent(mapply(logger::log_appender, old_log_appenders, old_log_namespaces))
 }
