d57f91ccffa10f036419c8cc40de2570a68f1f46
Maciej Nasinski <nasinski.maciej@gmail.com>
2022-01-13 19:46:20 +0100

    Update R/FilterStates.R

--- R/FilterStates.R
+++ R/FilterStates.R
@@ -1317,7 +1317,7 @@ MAEFilterStates <- R6::R6Class( # nolint
               logger::log_trace(
                 sprintf(
                   "MAEFilterStates$srv_add_filter_state@2 adding FilterState of variable %s, dataname: %s",
-                  input$var_to_add,
+                  deparse1(input$var_to_add),
                   deparse1(private$input_dataname)
                 )
               )
