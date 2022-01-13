89138657c6a0d51e0962d9fe105d18d213170e90
Maciej Nasinski <nasinski.maciej@gmail.com>
2022-01-13 20:31:40 +0100

    syntax

--- R/FilterState.R
+++ R/FilterState.R
@@ -557,7 +557,7 @@ FilterState <- R6::R6Class( # nolint
           deparse1(private$input_dataname),
           deparse1(self$get_varname()),
           paste(state$selected, collapse = " "),
-          deprase1(state$keep_na)
+          deparse1(state$keep_na)
         )
       )
       stopifnot(is.list(state) && all(names(state) %in% c("selected", "keep_na")))
