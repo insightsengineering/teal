d349c9dba631495f70265410921aee324dcea7ab
kpagacz <konrad.pagacz@gmail.com>
2022-02-16 15:24:25 +0100

    added a small missing test

--- tests/testthat/test-data_label.R
+++ tests/testthat/test-data_label.R
@@ -39,3 +39,11 @@ testthat::test_that("variable_labels<- assigns the labels to the columns", {
   variable_labels(custom_iris) <- colnames(custom_iris)
   testthat::expect_equal(get_variable_labels(custom_iris, fill = FALSE), stats::setNames(nm = colnames(custom_iris)))
 })
+
+testthat::test_that("get_labels' column_labels is a named vector of the labels of the passed data.frame", {
+  test <- data.frame(a = 1, b = 2)
+  test_labels <- c("testa", "testb")
+  attr(test[[1]], "label") <- test_labels[1]
+  attr(test[[2]], "label") <- test_labels[2]
+  testthat::expect_equal(get_labels(test)$column_labels, stats::setNames(object = test_labels, nm = colnames(test)))
+})
