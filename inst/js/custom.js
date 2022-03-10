3318463277a0bf9a07799e4399c88562082c0992
Dawid Kałędkowski <dawid.kaledkowski@gmail.com>
2022-03-10 07:45:48 +0100

    remove duplicated js (already in teal.data)

--- inst/js/custom.js
+++ inst/js/custom.js
@@ -14,21 +14,3 @@ function convertToDraggable(el) {
       $(el).trigger('end');
   }});
 }
-
-// allow a button to be pressed by pressing the enter key see
-// https://github.com/daattali/advanced-shiny/blob/master/proxy-click/app.R
-$(function() {
-  var $els = $("[data-proxy-click]");
-  $.each(
-    $els,
-    function(idx, el) {
-      var $el = $(el);
-      var $proxy = $("#" + $el.data("proxyClick"));
-      $el.keydown(function (e) {
-        if (e.keyCode == 13) {
-          $proxy.click();
-        }
-      });
-    }
-  );
-});
