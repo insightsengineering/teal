4c153fc945b35402524107f3048f939a4d4fe076
Nikolas Burkoff <nikolas.burkoff@roche.com>
2022-03-23 14:49:22 +0000

    add example module

---
+++ R/example_module.R
@@ -0,0 +1,38 @@
+#' An example `teal` module
+#'
+#' @param label `character`, the label of the module
+#' @return A `teal` module which can be included in the `modules` argument to [teal::init()].
+#' @examples
+#' app <- init(
+#'   data = teal_data(
+#'     dataset("IRIS", iris),
+#'     dataset("MTCARS", mtcars)
+#'   ),
+#'   modules = modules(example_module())
+#' )
+#' \dontrun{
+#' shinyApp(app$ui, app$server)
+#' }
+#' @export
+example_module <- function(label = "example teal module") {
+  checkmate::assert_string(label)
+  module(
+    label,
+    server = function(id, datasets) {
+      moduleServer(id, function(input, output, session) {
+        output$text <- renderText({
+          text <- utils::capture.output(print(datasets$get_data(input$dataname, filtered = TRUE)))
+          paste(text, collapse = "\n")
+        })
+      })
+    },
+    ui = function(id, datasets) {
+      ns <- NS(id)
+      teal.widgets::standard_layout(
+        output = verbatimTextOutput(ns("text")),
+        encoding = selectInput(ns("dataname"), "Choose a dataset", choices = datasets$datanames())
+      )
+    },
+    filters = "all"
+  )
+}
