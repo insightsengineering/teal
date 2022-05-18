35b317c0c0bab19106e49747daa70be45a1f7030
Nikolas Burkoff <nikolas.burkoff@roche.com>
2022-05-18 16:38:16 +0100

    reporter_previewer_module docs and validation

--- R/reporter_previewer_module.R
+++ R/reporter_previewer_module.R
@@ -1,11 +1,23 @@
+#' Create a `teal` module for previewing a report
+#'
+#' This function wraps [teal.reporter::reporter_previewer_ui()] and
+#' [teal.reporter::reporter_previewer_srv()] into a `teal_module` to be
+#' used in `teal` applications.
+#'
+#' If you are creating a `teal` application using [teal::init()] then this
+#' module will be added to your application automatically if any of your `teal modules`
+#' support report generation
+#'
+#' @inheritParams module
+#' @return `teal_module` containing the `teal.reporter` previewer functionality
 #' @export
 reporter_previewer_module <- function(label = "Report previewer") {
-
-  srv <- function(id, datasets, reporter, ...){
+  checkmate::assert_string(label)
+  srv <- function(id, datasets, reporter, ...) {
     teal.reporter::reporter_previewer_srv(id, reporter, ...)
   }
 
-  ui <- function(id, datasets, ...){
+  ui <- function(id, datasets, ...) {
     teal.reporter::reporter_previewer_ui(id, ...)
   }
 
