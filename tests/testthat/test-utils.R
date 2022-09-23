997c3d1afdeb9d600a0634154d0f1080d7b110a4
Dony Unardi <donyunardi@gmail.com>
2022-09-23 12:38:17 -0700

    add test cases
    adding test cases to increase coverage

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -3,4 +3,10 @@ testthat::test_that("get_teal_bs_theme", {
   withr::with_options(list("teal.bs_theme" = bslib::bs_theme(version = "5")), {
     testthat::expect_s3_class(get_teal_bs_theme(), "bs_theme")
   })
+  withr::with_options(list("teal.bs_theme" = 1), {
+    testthat::expect_warning(get_teal_bs_theme(), "the default shiny bootstrap is used")
+  })
+  withr::with_options(list("teal.bs_theme" = "bs_theme"), {
+    testthat::expect_warning(get_teal_bs_theme(), "the default shiny bootstrap is used")
+  })
 })
