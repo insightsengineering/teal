c61394601f126e03e1adf40a1d81994172e85b3e
Dawid Kałędkowski <6959016+gogonzo@users.noreply.github.com>
2022-11-14 11:57:53 +0100

    Fix failing test (#777)
    get_rcode_libraries tested dynamically

--- tests/testthat/test-module_nested_tabs.R
+++ tests/testthat/test-module_nested_tabs.R
@@ -315,13 +315,8 @@ testthat::test_that(".datasets_to_data returns tdata object", {
   testthat::expect_equal(
     isolate(get_code(data)),
     c(
-      "# Add any code to install/load your NEST environment here",
-      paste0(
-        "library(testthat)\nlibrary(shiny)\nlibrary(teal.data)\nlibrary(magrittr)\nlibrary(teal.transform)\n",
-        "library(teal)\nlibrary(matrixStats)\nlibrary(MatrixGenerics)\nlibrary(BiocGenerics)\nlibrary(S4Vectors)\n",
-        "library(IRanges)\nlibrary(GenomeInfoDb)\nlibrary(GenomicRanges)\nlibrary(Biobase)\n",
-        "library(SummarizedExperiment)\nlibrary(MultiAssayExperiment)"
-      ),
+      get_rcode_str_install(),
+      get_rcode_libraries(),
       "d1 <- data.frame(id = 1:5, pk = c(2, 3, 2, 1, 4), val = 1:5)\nd2 <- data.frame(id = 1:5, value = 1:5)\n\n",
       paste0(
         "stopifnot(rlang::hash(d1) == \"f6f90d2c133ca4abdeb2f7a7d85b731e\")\n",
