dfc37de41ae9129a9054fa224b68aa7e37fd7bf5
Aleksander Chlebowski <114988527+chlebowa@users.noreply.github.com>
2022-11-24 01:22:15 +0100

    Remove soft deprecated function `bookmarkableShinyApp` (#782)
    Closes [this](https://github.com/insightsengineering/teal/issues/756).
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- R/module_teal.R
+++ R/module_teal.R
@@ -157,36 +157,6 @@ srv_teal <- function(id, modules, raw_data, filter = list()) {
       }
     )
 
-    # bookmarking -----
-    # Note bookmarkableShinyApp has been soft deprecated and when it has been removed completely
-    # this code should be removed.
-    saved_datasets_state <- reactiveVal(NULL) # set when restored because data must already be populated
-    onBookmark(function(state) {
-      # this function is isolated by Shiny
-      # We store the entire R6 class with reactive values in it, but set the data to NULL.
-      # Note that we cannnot directly do this on datasets as this would trigger
-      # reactivity to recompute the filtered datasets, which is not needed.
-      logger::log_trace(
-        paste(
-          "srv_teal@2 saving active filter state for",
-          "datasets: { paste(names(datasets_reactive()$get_filter_state()), collapse = ' ') }."
-        )
-      )
-      state$values$datasets_state <- datasets_reactive()$get_filter_state()
-    })
-    onRestore(function(state) {
-      # The saved datasets mainly contains the filter states as the data
-      # was set to NULL before storing. The data should have been set again
-      # by the user, so we just need to set the filters.
-      logger::log_trace(
-        paste(
-          "srv_teal@3 restoring filter states from the bookmark for",
-          "datasets: { paste(names(state$values$datasets_state), collapse = ' ') }."
-        )
-      )
-      saved_datasets_state(state$values$datasets_state)
-    })
-
     # loading the data -----
     env <- environment()
     datasets_reactive <- reactive({
@@ -235,10 +205,6 @@ srv_teal <- function(id, modules, raw_data, filter = list()) {
         # have any effect as they are ignored when not present
         immediate = TRUE
       )
-
-      # switching filter to bookmarked state
-      if (!is.null(saved_datasets_state())) filter <- saved_datasets_state()
-
 
       # must make sure that this is only executed once as modules assume their observers are only
       # registered once (calling server functions twice would trigger observers twice each time)
