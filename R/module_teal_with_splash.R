c1a320921a0731ef984dd8cd4a5d84836a714904
Pawel Rucki <pawelrucki@gmail.com>
2023-01-12 21:25:39 +0100

    convert check to assert (#795)

--- R/module_teal_with_splash.R
+++ R/module_teal_with_splash.R
@@ -48,6 +48,9 @@ ui_teal_with_splash <- function(id,
 #' Please also refer to the doc of [init()].
 #'
 #' @inheritParams init
+#' @param modules `teal_modules` object containing the output modules which
+#'   will be displayed in the teal application. See [modules()] and [module()] for
+#'   more details.
 #' @inheritParams shiny::moduleServer
 #' @return `reactive`, return value of [srv_teal()]
 #' @export
