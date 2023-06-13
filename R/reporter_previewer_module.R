8a54c5276d10d86a83d8ae6527b93ddf05e043e0
go_gonzo <dawid.kaledkowski@gmail.com>
2023-06-13 13:51:33 +0200

    extend default report previewer

--- R/reporter_previewer_module.R
+++ R/reporter_previewer_module.R
@@ -10,10 +10,19 @@
 #' support report generation
 #'
 #' @inheritParams module
+#' @param srv_args (`named list`)\cr
+#'  Arguments passed to [teal.reporter::reporter_previewer_srv()].
+#' @param ui_args (`named list`)\cr
+#'  Arguments passed to [teal.reporter::reporter_previewer_ui()].
 #' @return `teal_module` containing the `teal.reporter` previewer functionality
 #' @export
-reporter_previewer_module <- function(label = "Report previewer") {
+reporter_previewer_module <- function(label = "Report previewer", srv_args = list(), ui_args = list()) {
   checkmate::assert_string(label)
+  checkmate::assert_list(srv_args, names = "named")
+  checkmate::assert_list(ui_args, names = "named")
+  checkmate::assert_true(all(names(srv_args) %in% names(formals(teal.reporter::reporter_previewer_srv))))
+  checkmate::assert_true(all(names(ui_args) %in% names(formals(teal.reporter::reporter_previewer_ui))))
+
   srv <- function(id, reporter, ...) {
     teal.reporter::reporter_previewer_srv(id, reporter, ...)
   }
@@ -22,9 +31,11 @@ reporter_previewer_module <- function(label = "Report previewer") {
     teal.reporter::reporter_previewer_ui(id, ...)
   }
 
-  module(
+  module <- module(
     label = label,
     server = srv, ui = ui,
-    server_args = list(), ui_args = list(), filters = NULL
+    server_args = srv_args, ui_args = ui_args, filters = NULL
   )
+  class(module) <- c("teal_module_previewer", class(module))
+  module
 }
