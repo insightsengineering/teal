032c1a2b956f9567e6d26b70429887950a8e8e3a
unknown <kirar.kartikeya1@gmail.com>
2023-10-12 13:41:30 +0530

    Adding card template function to be used across different module. and unit-test.

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -10,3 +10,25 @@ testthat::test_that("get_teal_bs_theme", {
     testthat::expect_warning(get_teal_bs_theme(), "the default shiny bootstrap is used")
   })
 })
+
+testthat::test_that("card_template function returns TealReportCard object with appropriate content and labels", {
+  fd <- teal.slice::init_filtered_data(list(iris = list(dataset = iris)))
+  filter_panel_api <- teal.slice::FilterPanelAPI$new(fd)
+
+  card <- shiny::isolate(card_template(title = "Card title",
+                                       label ="Card label",
+                                       description = "Sample description",
+                                       with_filter = TRUE,
+                                       filter_panel_api =  filter_panel_api))
+  testthat::expect_s3_class(card, c("TealReportCard"))
+  testthat::expect_equal(card$get_name(), "Card label")
+  testthat::expect_length(card$get_content(), 4)
+
+  card <- shiny::isolate(card_template(title = "Card title",
+                                       label =  "",
+                                       with_filter = FALSE,
+                                       filter_panel_api = filter_panel_api))
+  testthat::expect_s3_class(card, c("TealReportCard"))
+  testthat::expect_equal(card$get_name(), "Card title")
+  testthat::expect_length(card$get_content(), 1)
+})
