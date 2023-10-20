8df71ba3f9f13acd30f4ba63cb41a4782dc7696e
Aleksander Chlebowski <114988527+chlebowa@users.noreply.github.com>
2023-10-20 16:51:01 +0200

    928 upload snapshot file (#929)
    Closes #928 
    
    Added possibility to upload a snapshot file to the snapshot manager
    module.
    The snapshot manager control bar (top row in table) receives a third
    button. Clicking it opens a modal dialog that allows for selecting a
    `.json` file to upload and naming the snapshot.
    The new snapshot is added to the snapshot list and the filter manager
    dialog is reopened so that the user may immediately apply the new
    snapshot.
    
    To ensure that the uploaded snapshot matches the app, `teal_slices`
    class receives the `app_id` attribute, which can be set with the
    `app_id` argument to `teal::teal_slices`. The attribute will be set by
    `init` to stamp a `teal_slices`. The attribute will be stored along with
    other attributes. Upon uploading a snapshot, the app id of the upload
    will be compared to that of the app.
    
    `slices_restore` had to be re-defined in the `teal` namespace because it
    calls `teal_slices` and that must be the one from `teal`.
    
    
    #### TESTING
    ```
    options(teal.log_level = "WARN", teal.show_js_log = FALSE)
    
    library(teal.modules.general)
    pkgload::load_all("../teal.slice")
    pkgload::load_all("../teal")
    
    rm(list = ls())
    
    funny_module <- function (label = "Filter states", datanames = "all") {
      checkmate::assert_string(label)
      module(
        label = label,
        datanames = datanames,
        ui = function(id, ...) {
          ns <- NS(id)
          div(
            h2("The following filter calls are generated:"),
            verbatimTextOutput(ns("filter_states")),
            verbatimTextOutput(ns("filter_calls")),
            actionButton(ns("reset"), "reset_to_default")
          )
        },
        server = function(input, output, session, data, filter_panel_api) {
          checkmate::assert_class(data, "tdata")
          observeEvent(input$reset, set_filter_state(filter_panel_api, default_filters))
          output$filter_states <-  renderPrint({
            logger::log_trace("rendering text1")
            filter_panel_api %>% get_filter_state()
          })
          output$filter_calls <- renderText({
            logger::log_trace("rendering text2")
            attr(data, "code")()
          })
        }
      )
    }
    
    dead_module <- function(label = "empty module", datanames = NULL) {
      module(label = label,
             datanames = datanames,
             ui = function(id) {
               ns <- NS(id)
               tagList(
                 h4("this is just text")
               )
             },
             server = function(id, filter_panel_api, reporter) {
               message("hello")
             }
      )
    }
    
    default_filters <- teal::teal_slices(
      teal_slice("iris", "Sepal.Length"),
      teal_slice("iris", "Sepal.Width"),
      teal_slice("iris", "Species", fixed = TRUE),
      teal_slice("mtcars", "mpg"),
      exclude_varnames = list(
        iris = c("Petal.Length"),
        mtcars = c("qsec", "drat")
      ),
      module_specific = TRUE,
      mapping = list(
        table = "iris Species",
        funny1 = c("iris Sepal.Length", "iris Sepal.Width", "iris Species"),
        funny2 = "iris Species",
        global_filters = "mtcars mpg"
      )
    )
    
    app <- init(
      data = teal_data(
        dataset("iris", iris),
        dataset("mtcars", mtcars)
      ),
      modules = modules(
        tm_data_table(
          "table",
          variables_selected = list(),
          dt_args = list()
        ),
        modules(
          label = "tab1",
          funny_module("funny1", datanames = NULL),
          funny_module("funny2", datanames = "iris"),
          dead_module("empty", datanames = NULL)
        )
      ),
      filter = default_filters
    )
    
    shinyApp(app$ui, app$server, options = list(launch.browser = TRUE))
    ```
    
    ---------
    
    Signed-off-by: Aleksander Chlebowski <114988527+chlebowa@users.noreply.github.com>
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- tests/testthat/test-init.R
+++ tests/testthat/test-init.R
@@ -9,8 +9,8 @@ mods <- teal:::example_modules()
 testthat::test_that("init data accepts TealData objects", {
   teal_data_object <- teal.data::teal_data(dataset_1)
   cdisc_data_object <- teal.data::cdisc_data(adsl_dataset)
-  testthat::expect_error(init(data = teal_data_object, modules = mods), NA)
-  testthat::expect_error(init(data = cdisc_data_object, modules = mods), NA)
+  testthat::expect_no_error(init(data = teal_data_object, modules = mods))
+  testthat::expect_no_error(init(data = cdisc_data_object, modules = mods))
 })
 
 testthat::test_that("init data throws an error with input other than accepted input", {
@@ -27,16 +27,15 @@ testthat::test_that("init data throws an error with input other than accepted in
 })
 
 testthat::test_that("init data accepts a single TealDataset/CDISCTealDataset", {
-  testthat::expect_error(init(data = teal.data::dataset("iris", head(iris)), modules = mods), NA)
-  testthat::expect_error(
+  testthat::expect_no_error(init(data = teal.data::dataset("iris", head(iris)), modules = mods))
+  testthat::expect_no_error(
     init(
       data = teal.data::cdisc_dataset("ADSL", adsl_df, parent = character(0), keys = teal.data::get_cdisc_keys("ADSL")),
       modules = mods
-    ),
-    NA
+    )
   )
-  testthat::expect_error(init(data = dataset_1, modules = mods), NA)
-  testthat::expect_error(init(data = adsl_dataset, modules = mods), NA)
+  testthat::expect_no_error(init(data = dataset_1, modules = mods))
+  testthat::expect_no_error(init(data = adsl_dataset, modules = mods))
 })
 
 testthat::test_that("init data accepts a list of single TealDataset/CDISCTealDataset without renaming", {
@@ -45,104 +44,112 @@ testthat::test_that("init data accepts a list of single TealDataset/CDISCTealDat
     teal.data::cdisc_dataset("ADSL", adsl_df, parent = character(0), keys = teal.data::get_cdisc_keys("ADSL"))
   )
 
-  testthat::expect_error(init(data = list(teal.data::dataset("iris", head(iris))), modules = mods), NA)
-  testthat::expect_error(init(
-    data = list(
-      teal.data::cdisc_dataset("ADSL", adsl_df, parent = character(0), keys = teal.data::get_cdisc_keys("ADSL"))
-    ),
-    modules = mods
-  ), NA)
-  testthat::expect_error(init(data = dataset_list, modules = mods), NA)
-  testthat::expect_error(init(data = cdisc_dataset_list, modules = mods), NA)
+  testthat::expect_no_error(init(data = list(teal.data::dataset("iris", head(iris))), modules = mods))
+  testthat::expect_no_error(
+    init(
+      data = list(
+        teal.data::cdisc_dataset("ADSL", adsl_df, parent = character(0), keys = teal.data::get_cdisc_keys("ADSL"))
+      ),
+      modules = mods
+    )
+  )
+  testthat::expect_no_error(init(data = dataset_list, modules = mods))
+  testthat::expect_no_error(init(data = cdisc_dataset_list, modules = mods))
 })
 
 testthat::test_that("init data accepts a single dataframe", {
-  testthat::expect_error(init(data = adsl_df, modules = mods), NA)
+  testthat::expect_no_error(init(data = adsl_df, modules = mods))
 })
 
 testthat::test_that("init data accepts a list of single dataframe without renaming", {
-  testthat::expect_error(init(data = list(adsl_df), modules = mods), NA)
+  testthat::expect_no_error(init(data = list(adsl_df), modules = mods))
 })
 
 testthat::test_that("init data accepts a list of single dataframe with renaming", {
   adsl_list <- list(data1 = adsl_df)
-  testthat::expect_error(init(data = list(data1 = adsl_df), modules = mods), NA)
-  testthat::expect_error(init(data = adsl_list, modules = mods), NA)
+  testthat::expect_no_error(init(data = list(data1 = adsl_df), modules = mods))
+  testthat::expect_no_error(init(data = adsl_list, modules = mods))
 })
 
 testthat::test_that("init data accepts a list of a TealDataset and a dataframe without renaming", {
-  testthat::expect_error(init(data = list(dataset_1, adsl_df), modules = mods), NA)
+  testthat::expect_no_error(init(data = list(dataset_1, adsl_df), modules = mods))
 })
 
 testthat::test_that("init data accepts a single MultiAssayExperiment object", {
   utils::data(miniACC, package = "MultiAssayExperiment")
-  testthat::expect_error(init(data = miniACC, modules = mods), NA)
+  testthat::expect_no_error(init(data = miniACC, modules = mods))
 })
 
 testthat::test_that("init data accepts a list of a single MultiAssayExperiment object without renaming", {
   utils::data(miniACC, package = "MultiAssayExperiment")
-  testthat::expect_error(init(data = list(miniACC), modules = mods), NA)
+  testthat::expect_no_error(init(data = list(miniACC), modules = mods))
 })
 
 testthat::test_that("init data accepts a list of a single MultiAssayExperiment object with renaming", {
   utils::data(miniACC, package = "MultiAssayExperiment")
-  testthat::expect_error(init(data = list(x = miniACC), modules = mods), NA)
+  testthat::expect_no_error(init(data = list(x = miniACC), modules = mods))
 })
 
 testthat::test_that("init data acceptsa mixed list of MultiAssayExperiment object and data.frame", {
   utils::data(miniACC, package = "MultiAssayExperiment")
-  testthat::expect_error(init(data = list(x = miniACC, y = head(iris)), modules = mods), NA)
+  testthat::expect_no_error(init(data = list(x = miniACC, y = head(iris)), modules = mods))
 })
 
 testthat::test_that("init data accepts a list of a TealDataset and a dataframe with renaming", {
-  testthat::expect_error(init(
-    data = list(
-      data1 = teal.data::dataset("iris", head(iris)),
-      data2 = as.data.frame(as.list(setNames(nm = teal.data::get_cdisc_keys("ADSL"))))
-    ),
-    modules = mods
-  ), NA)
-  testthat::expect_error(init(data = list(data1 = dataset_1, data2 = adsl_df), modules = mods), NA)
+  testthat::expect_no_error(
+    init(
+      data = list(
+        data1 = teal.data::dataset("iris", head(iris)),
+        data2 = as.data.frame(as.list(setNames(nm = teal.data::get_cdisc_keys("ADSL"))))
+      ),
+      modules = mods
+    )
+  )
+  testthat::expect_no_error(init(data = list(data1 = dataset_1, data2 = adsl_df), modules = mods))
 })
 
 testthat::test_that("init data accepts a list of mixed TealDataset and dataframe with mixed renaming", {
-  testthat::expect_error(init(data = list(data1 = teal.data::dataset("iris", head(iris)), adsl_df), modules = mods), NA)
-  testthat::expect_error(init(data = list(dataset_1, data2 = adsl_df), modules = mods), NA)
+  testthat::expect_no_error(init(data = list(data1 = teal.data::dataset("iris", head(iris)), adsl_df), modules = mods))
+  testthat::expect_no_error(init(data = list(dataset_1, data2 = adsl_df), modules = mods))
 })
 
 testthat::test_that("init data accepts TealDatasetConnector object", {
   dsc1 <- teal.data::dataset_connector("iris", teal.data::callable_function(function() head(iris)))
-  testthat::expect_error(init(data = dsc1, modules = mods), NA)
-  testthat::expect_error(init(
-    data = teal.data::dataset_connector("iris", teal.data::callable_function(function() head(iris))),
-    modules = mods
-  ), NA)
+  testthat::expect_no_error(init(data = dsc1, modules = mods))
+  testthat::expect_no_error(
+    init(
+      data = teal.data::dataset_connector("iris", teal.data::callable_function(function() head(iris))),
+      modules = mods
+    )
+  )
 })
 
 testthat::test_that("init data accepts a list of TealDatasetConnector object", {
   dsc1 <- list(teal.data::dataset_connector("iris", teal.data::callable_function(function() head(iris))))
-  testthat::expect_error(init(data = dsc1, modules = mods), NA)
-  testthat::expect_error(
-    init(data = list(
-      teal.data::dataset_connector("iris", teal.data::callable_function(function() head(iris)))
-    ), modules = mods),
-    NA
+  testthat::expect_no_error(init(data = dsc1, modules = mods))
+  testthat::expect_no_error(
+    init(
+      data = list(
+        teal.data::dataset_connector("iris", teal.data::callable_function(function() head(iris)))
+      ),
+      modules = mods
+    )
   )
 })
 
 testthat::test_that("init modules accepts a teal_modules object", {
   mods <- modules(example_module(), example_module())
-  testthat::expect_error(init(data = iris, modules = mods), NA)
+  testthat::expect_no_error(init(data = iris, modules = mods))
 })
 
 testthat::test_that("init modules accepts a list of teal_module elements", {
   mods <- list(example_module(), example_module())
-  testthat::expect_error(init(data = iris, modules = mods), NA)
+  testthat::expect_no_error(init(data = iris, modules = mods))
 })
 
 testthat::test_that("init modules accepts a teal_module object", {
   mods <- example_module()
-  testthat::expect_error(init(data = iris, modules = mods), NA)
+  testthat::expect_no_error(init(data = iris, modules = mods))
 })
 
 testthat::test_that("init filter accepts named list or `teal_slices`", {
