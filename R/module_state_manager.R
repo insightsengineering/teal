9e9c9decf96147e0238a2a8c700124116fedee92
Aleksander Chlebowski <aleksander.chlebowski@contractors.roche.com>
2023-12-15 11:54:55 +0100

    rebuild module

--- R/module_state_manager.R
+++ R/module_state_manager.R
@@ -38,6 +38,7 @@ state_manager_srv <- function(id, slices_global, mapping_matrix, filtered_data_l
 
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
+    sesh <- get_master_session()
 
     # Store initial input states.
     grab_history <- reactiveVal({
@@ -46,21 +47,44 @@ state_manager_srv <- function(id, slices_global, mapping_matrix, filtered_data_l
       )
     })
 
-    # Grab current input state - name grab.
-    observeEvent(input$grab_add, {
-      showModal(
-        modalDialog(
-          textInput(ns("grab_name"), "Name the grab", width = "100%", placeholder = "Meaningful, unique name"),
-          footer = tagList(
-            actionButton(ns("grab_name_accept"), "Accept", icon = icon("thumbs-up")),
-            modalButton(label = "Cancel", icon = icon("thumbs-down"))
-          ),
-          size = "s"
-        )
-      )
+    # 3. arrange restoring filter state after restoring bookmark
+    ### work in progress
+    sesh$onBookmark(function(state) {
+      # 1. get input names and isolate filter panel
+      filter_panel_inputs <- grep("filter_panel", names(sesh$input), value = TRUE)
+      # 2. exclude filter panel from bookmark
+      sesh$setBookmarkExclude(character(0L))
+      sesh$setBookmarkExclude(filter_panel_inputs)
+
+      ### smth like this should happen:
+      snapshot <- as.list(slices_global(), recursive = TRUE)
+      attr(snapshot, "mapping") <- matrix_to_mapping(mapping_matrix())
+      state$values$filter_state_on_bookmark <- snapshot
+      ### end; requires access to slices_global and mapping_matrix
+      state$values$snapshot_history <- snapshot_history()   # isolate this?
+      state$values$grab_history <- grab_history()           # isolate this?
     })
-    # Grab current input state - store grab.
-    observeEvent(input$grab_name_accept, {
+    sesh$onRestored(function(state) {
+      ### smth like this should happen:
+      snapshot <- state$values$filter_state_on_bookmark
+      snapshot_state <- as.teal_slices(snapshot)
+      mapping_unfolded <- unfold_mapping(attr(snapshot_state, "mapping"), names(filtered_data_list))
+      mapply(
+        function(filtered_data, filter_ids) {
+          filtered_data$clear_filter_states(force = TRUE)
+          slices <- Filter(function(x) x$id %in% filter_ids, snapshot_state)
+          filtered_data$set_filter_state(slices)
+        },
+        filtered_data = filtered_data_list,
+        filter_ids = mapping_unfolded
+      )
+      slices_global(snapshot_state)
+      ### end; requires access to slices_global and filtered_data_list
+      snapshot_history(state$values$snapshot_history)
+      grab_history(state$values$grab_history)
+    })
+
+    sesh$onBookmarked(function(url) {
       grab_name <- trimws(input$grab_name)
       if (identical(grab_name, "")) {
         showNotification(
@@ -68,51 +92,15 @@ state_manager_srv <- function(id, slices_global, mapping_matrix, filtered_data_l
           type = "message"
         )
         updateTextInput(inputId = "grab_name", value = "", placeholder = "Meaningful, unique name")
+        unlink(strsplit(url, "_state_id_=")[[1L]][[2L]], recursive = TRUE, force = TRUE, expand = FALSE)
       } else if (is.element(make.names(grab_name), make.names(names(grab_history())))) {
         showNotification(
           "This name is in conflict with other grab names. Please choose a different one.",
           type = "message"
         )
         updateTextInput(inputId = "grab_name", value = , placeholder = "Meaningful, unique name")
+        unlink(strsplit(url, "_state_id_=")[[1L]][[2L]], recursive = TRUE, force = TRUE, expand = FALSE)
       } else {
-        sesh <- get_master_session()
-        # 1. get input names and isolate filter panel
-        filter_panel_inputs <- grep("filter_panel", names(sesh$input), value = TRUE)
-        # 2. exclude filter panel from bookmark
-        sesh$setBookmarkExclude(character(0L))
-        sesh$setBookmarkExclude(filter_panel_inputs)
-        # 3. arrange restoring filter state after restoring bookmark
-        ### work in progress
-        sesh$onBookmark(function(state) {
-          ### smth like this should happen:
-          snapshot <- as.list(slices_global(), recursive = TRUE)
-          attr(snapshot, "mapping") <- matrix_to_mapping(mapping_matrix())
-          state$values$filter_state_on_bookmark <- snapshot
-          ### end; requires access to slices_global and mapping_matrix
-          state$values$snapshot_history <- snapshot_history()   # isolate this?
-          state$values$grab_history <- grab_history()           # isolate this?
-        })
-        sesh$onRestored(function(state) {
-          ### smth like this should happen:
-          snapshot <- state$values$filter_state_on_bookmark
-          snapshot_state <- as.teal_slices(snapshot)
-          mapping_unfolded <- unfold_mapping(attr(snapshot_state, "mapping"), names(filtered_data_list))
-          mapply(
-            function(filtered_data, filter_ids) {
-              filtered_data$clear_filter_states(force = TRUE)
-              slices <- Filter(function(x) x$id %in% filter_ids, snapshot_state)
-              filtered_data$set_filter_state(slices)
-            },
-            filtered_data = filtered_data_list,
-            filter_ids = mapping_unfolded
-          )
-          slices_global(snapshot_state)
-          ### end; requires access to slices_global and filtered_data_list
-          snapshot_history(state$values$snapshot_history)
-          grab_history(state$values$grab_history)
-        })
-        # 4. do bookmark
-        url <- grab_state(sesh)
         # 5. add bookmark URL to grab history (with name)
         grab_update <- c(grab_history(), list(url))
         names(grab_update)[length(grab_update)] <- grab_name
@@ -122,6 +110,21 @@ state_manager_srv <- function(id, slices_global, mapping_matrix, filtered_data_l
         # Reopen filter manager modal by clicking button in the main application.
         shinyjs::click(id = "teal-main_ui-filter_manager-show", asis = TRUE)
       }
+    })
+
+    # Grab current input state - name grab.
+    observeEvent(input$grab_add, {
+      showModal(
+        modalDialog(
+          textInput(ns("grab_name"), "Name the grab", width = "100%", placeholder = "Meaningful, unique name"),
+          footer = tagList(
+            # actionButton(ns("grab_name_accept"), "Accept", icon = icon("thumbs-up")),
+            bookmarkButton("Accept", icon = icon("thumbs-up")),
+            modalButton(label = "Cancel", icon = icon("thumbs-down"))
+          ),
+          size = "s"
+        )
+      )
     })
 
     # Create UI elements and server logic for the grab table.
