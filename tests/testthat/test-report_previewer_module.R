404ef5ae73d80fb83d2d448e096b454c3eb93c87
André Veríssimo <211358+averissimo@users.noreply.github.com>
2024-01-23 13:36:31 +0100

    lintr: split long lines

--- tests/testthat/test-report_previewer_module.R
+++ tests/testthat/test-report_previewer_module.R
@@ -1,6 +1,10 @@
 testthat::test_that("report_previewer_module throws error if label is not string", {
-  testthat::expect_error(reporter_previewer_module(label = 5), "Assertion on 'label' failed: Must be of type 'string'")
-  testthat::expect_error(reporter_previewer_module(label = c("A", "B")), "Assertion on 'label' failed: Must have length 1.")
+  testthat::expect_error(
+    reporter_previewer_module(label = 5), "Assertion on 'label' failed: Must be of type 'string'"
+  )
+  testthat::expect_error(
+    reporter_previewer_module(label = c("A", "B")), "Assertion on 'label' failed: Must have length 1."
+  )
 })
 
 testthat::test_that("report_previewer_module throws no error and stores label if label is string", {
