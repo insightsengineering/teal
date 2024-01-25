aa01b8c20da983176dc82c4bad38ce0917c29d13
m7pr <marcin.kosinski.mk1@roche.com>
2024-01-25 14:39:18 +0100

    ... for print.teal_module is actually ignored

--- R/modules.R
+++ R/modules.R
@@ -449,7 +449,7 @@ print.teal_modules <- function(x, ...) {
 
 #' Prints `teal_module`
 #' @param x (`teal_module`)
-#' @param ... arguments passed to other methods.
+#' @param ... ignored
 #' @export
 #' @rdname module
 print.teal_module <- print.teal_modules
