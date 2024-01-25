bac378a83076161cfaf0e4b3a8b8e70dbbb61a48
unknown <kirar.kartikeya1@gmail.com>
2024-01-25 14:25:29 +0530

    fix typo

--- R/module_teal_with_splash.R
+++ R/module_teal_with_splash.R
@@ -9,7 +9,7 @@
 #' user interaction, particularly during the initial heavy data processing or
 #' when querying for a password to fetch data.
 #'
-#' [`init()`] is a very wrapper around this module useful for end-users which
+#' [`init()`] is a wrapper around this module useful for end-users which
 #' assumes that it is a top-level module and cannot be embedded.
 #' This function instead adheres to the `shiny` module conventions.
 #'
