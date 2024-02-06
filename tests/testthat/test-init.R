f1ee4ece32712b7742fa8df1e11e8a42a0c719c9
Aleksander Chlebowski <aleksander.chlebowski@contractors.roche.com>
2024-02-06 14:17:43 +0100

    add missing prefixes in test files

--- tests/testthat/test-init.R
+++ tests/testthat/test-init.R
@@ -23,17 +23,17 @@ testthat::test_that("init data accepts teal_data_module", {
 # modules ----
 testthat::test_that("init modules accepts a teal_modules object", {
   mods <- modules(example_module(), example_module())
-  testthat::expect_no_error(init(data = teal_data(iris = iris), modules = mods))
+  testthat::expect_no_error(init(data = teal.data::teal_data(iris = iris), modules = mods))
 })
 
 testthat::test_that("init modules accepts a list of teal_module elements", {
   mods <- list(example_module(), example_module())
-  testthat::expect_no_error(init(data = teal_data(iris = iris), modules = mods))
+  testthat::expect_no_error(init(data = teal.data::teal_data(iris = iris), modules = mods))
 })
 
 testthat::test_that("init modules accepts a teal_module object", {
   mods <- example_module()
-  testthat::expect_no_error(init(data = teal_data(iris = iris), modules = mods))
+  testthat::expect_no_error(init(data = teal.data::teal_data(iris = iris), modules = mods))
 })
 
 # filter ----
@@ -41,9 +41,11 @@ testthat::test_that("init filter accepts `teal_slices`", {
   fs <- teal.slice::teal_slices(
     teal.slice::teal_slice(dataname = "iris", varname = "species", selected = "setosa")
   )
-  testthat::expect_no_error(init(data = teal_data(iris = iris), modules = modules(example_module()), filter = fs))
+  testthat::expect_no_error(
+    init(data = teal.data::teal_data(iris = iris), modules = modules(example_module()), filter = fs)
+  )
   testthat::expect_error(
-    init(data = teal_data(iris = iris), modules = modules(example_module()), filter = unclass(fs)),
+    init(data = teal.data::teal_data(iris = iris), modules = modules(example_module()), filter = unclass(fs)),
     "Assertion on 'filter' failed"
   )
 })
@@ -51,7 +53,7 @@ testthat::test_that("init filter accepts `teal_slices`", {
 # data + modules ----
 testthat::test_that("init throws when an empty `data` is used", {
   testthat::expect_error(
-    init(data = teal_data(), modules = list(example_module())),
+    init(data = teal.data::teal_data(), modules = list(example_module())),
     "The environment of `data` is empty."
   )
 })
@@ -60,7 +62,7 @@ testthat::test_that("init throws when datanames in modules incompatible w/ datan
   msg <- "Module 'example teal module' uses datanames not available in 'data'"
   testthat::expect_error(
     init(
-      data = teal_data(mtcars = mtcars),
+      data = teal.data::teal_data(mtcars = mtcars),
       modules = list(example_module(datanames = "iris"))
     ),
     "Module 'example teal module' uses datanames not available in 'data'"
@@ -70,7 +72,7 @@ testthat::test_that("init throws when datanames in modules incompatible w/ datan
 testthat::test_that("init throws when dataname in filter incompatible w/ datanames in data", {
   testthat::expect_warning(
     init(
-      data = teal_data(mtcars = mtcars),
+      data = teal.data::teal_data(mtcars = mtcars),
       modules = modules(example_module()),
       filter = teal_slices(teal_slice(dataname = "iris", varname = "Species"))
     ),
--- tests/testthat/test-tdata.R
+++ tests/testthat/test-tdata.R
@@ -218,7 +218,7 @@ testthat::test_that("join_keys returns join_keys object if it exists inside tdat
 code <- c("iris <- iris", "mtcars <- mtcars")
 data_tdata <- teal::new_tdata(list(iris = iris, mtcars = mtcars), code)
 data_teal_data <- teal.data::teal_data(iris = iris, mtcars = mtcars, code = code)
-data_reactive <- reactive(teal.data::teal_data(iris = iris, mtcars = mtcars, code = code))
+data_reactive <- shiny::reactive(teal.data::teal_data(iris = iris, mtcars = mtcars, code = code))
 
 testthat::test_that("as_tdata accepts all possible inputs", {
   testthat::expect_no_error(as_tdata(data_tdata))
