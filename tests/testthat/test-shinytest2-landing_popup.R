fa0f3e19dc083eb6ca36f2e0b2fa1c2138dc7ce4
m7pr <marcin.kosinski.mk1@roche.com>
2024-03-08 13:28:31 +0100

    2 more simple tests for landing_popup_module

--- tests/testthat/test-shinytest2-landing_popup.R
+++ tests/testthat/test-shinytest2-landing_popup.R
@@ -16,7 +16,67 @@ testthat::test_that("e2e: teal app with landing_popup_module initializes with no
   app$stop()
 })
 
-test_that("e2e: app with landing_popup_module creates modal containing specified title, content and buttons", {
+extract_onclick <- function(id) {
+  app$get_html(id) %>%
+    rvest::read_html() %>%
+    rvest::html_nodes("button") %>%
+    rvest::html_attr("onclick")
+}
+extract_text <- function(id) {
+  app$get_html(id) %>%
+    rvest::read_html() %>%
+    rvest::html_text() %>%
+    trimws()
+}
+phash <- function(text) paste0("#", text)
+
+test_that("e2e: app with default landing_popup_module creates modal containing specified title, content and buttons", {
+  app <- TealAppDriver$new(
+    data = simple_teal_data(),
+    modules = modules(
+      landing_popup_module(),
+      example_module()
+    )
+  )
+  app$wait_for_idle(timeout = default_idle_timeout)
+
+  expect_equal(
+    app$get_html(".btn-default") %>%
+      grep("Accept", x = ., value = TRUE) %>%
+      rvest::read_html() %>%
+      rvest::html_text(),
+    "Accept"
+  )
+
+  app$stop()
+})
+
+test_that("e2e: when default landing_popup_module is closed, it shows the underlying teal app", {
+  app <- TealAppDriver$new(
+    data = simple_teal_data(),
+    modules = modules(
+      landing_popup_module(),
+      example_module()
+    )
+  )
+  app$wait_for_idle(timeout = default_idle_timeout)
+
+  # TODO: Unsure it actually clicks the Approve button of landing_popup_module()
+  app$click(selector = ".btn-default:nth-child(1)")
+
+  # QUESTION: Is there a better way of checking the name of current module?
+  expect_equal(
+    app$get_html("#teal-main_ui-root-active_tab") %>%
+      rvest::read_html() %>%
+      rvest::html_node("a") %>%
+      rvest::html_attr("data-value"),
+    "example_teal_data"
+  )
+
+  app$stop()
+})
+
+test_that("e2e: app with customized landing_popup_module creates modal containing specified title, content and buttons", {
   modal_title <- "Custom Landing Popup Module Title"
   modal_content_message <- "A welcome message!"
   modal_content <- tags$b(modal_content_message, style = "color: red;")
@@ -55,20 +115,6 @@ test_that("e2e: app with landing_popup_module creates modal containing specified
 
   app$wait_for_idle(timeout = default_idle_timeout)
 
-  extract_onclick <- function(id) {
-    app$get_html(id) %>%
-      rvest::read_html() %>%
-      rvest::html_nodes("button") %>%
-      rvest::html_attr("onclick")
-  }
-  extract_text <- function(id) {
-    app$get_html(id) %>%
-      rvest::read_html() %>%
-      rvest::html_text() %>%
-      trimws()
-  }
-  phash <- function(text) paste0("#", text)
-
   expect_equal(
     extract_text(".modal-title"),
     modal_title
@@ -105,4 +151,30 @@ test_that("e2e: app with landing_popup_module creates modal containing specified
   )
 
   app$stop()
+})
+
+test_that("e2e: when customized button in landing_popup_module is clicked, it redirects to a certain page", {
+
+  skip("It actually did not clicked the button.")
+  app <- TealAppDriver$new(
+    data = simple_teal_data(),
+    modules = modules(
+      landing_popup_module(
+        buttons = actionButton("read", "Read more", onclick = "window.open('http://google.com', '_blank')")
+      ),
+      example_module()
+    )
+  )
+  app$wait_for_idle(timeout = default_idle_timeout)
+
+  app$click(selector = "#read")
+  # app$get_screenshot() still shows the app... :/
+
+  expect_equal(
+    app$get_url(),
+    "https://www.google.com/"
+  )
+
+  app$stop()
+
 })
