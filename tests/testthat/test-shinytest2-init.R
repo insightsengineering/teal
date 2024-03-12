917751ab9efd509ee39aa713067c91943dd1ea1c
m7pr <marcin.kosinski.mk1@roche.com>
2024-03-12 11:13:14 +0100

    test for sessionInfo modal

--- tests/testthat/test-shinytest2-init.R
+++ tests/testthat/test-shinytest2-init.R
@@ -8,6 +8,67 @@ testthat::test_that("e2e: teal app initializes with no errors", {
   app$stop()
 })
 
+testthat::test_that("e2e: teal app initializes with sessionInfo modal", {
+  app <- TealAppDriver$new(
+    data = simple_teal_data(),
+    modules = example_module(label = "Example Module")
+  )
+  app$wait_for_idle(timeout = default_idle_timeout)
+
+  # Check if button exists.
+  testthat::expect_equal(
+    app$get_text("#teal-sessionInfo-button"),
+    "Session Info"
+  )
+
+  app$click(selector = "#teal-sessionInfo-button")
+
+  # Check header and title content.
+  testthat::expect_equal(
+    app$get_text("#shiny-modal > div > div > div.modal-header > h4"),
+    "SessionInfo"
+  )
+  testthat::expect_equal(
+    app$get_text("#teal-sessionInfo-copy_button1"),
+    "Copy to Clipboard"
+  )
+  testthat::expect_equal(
+    app$get_text("#shiny-modal > div > div > div.modal-body > div > button:nth-child(2)"),
+    "Dismiss"
+  )
+
+  # Check session info output.
+  session_info <- app$get_text("#teal-sessionInfo-verbatim_content")
+
+  greplf <- function(text, content = session_info) {
+    grepl(text, content, fixed = TRUE)
+  }
+
+  testthat::expect_true(greplf("R version"))
+  testthat::expect_true(greplf("attached base packages:"))
+  testthat::expect_true(greplf("loaded via a namespace (and not attached):"))
+
+  testthat::expect_true(greplf("shiny"))
+  testthat::expect_true(greplf("teal.slice"))
+  testthat::expect_true(greplf("teal.reporter"))
+
+  # Check footer buttons.
+  testthat::expect_equal(
+    app$get_text("#teal-sessionInfo-copy_button2"),
+    "Copy to Clipboard"
+  )
+  testthat::expect_equal(
+    app$get_text("#shiny-modal > div > div > div.modal-footer > button:nth-child(2)"),
+    "Dismiss"
+  )
+
+  app$stop()
+})
+
+
+
+
+
 testthat::test_that("e2e: init creates UI containing specified title, favicon, header and footer", {
   app_title <- "Custom Teal App Title"
   app_favicon <- "https://raw.githubusercontent.com/insightsengineering/hex-stickers/main/PNG/teal.png"
