c2f0ed6010749f35ea3f96c32a21449634a6a3d8
vedhav <vedhaviyash4@gmail.com>
2024-03-12 15:10:54 +0530

    test: check that module server gets executed only when module becomes active

--- tests/testthat/test-shinytest2-modules.R
+++ tests/testthat/test-shinytest2-modules.R
@@ -1,3 +1,41 @@
+testthat::test_that("e2e: the module server logic is only triggered when the teal module becomes active", {
+  value_export_module <- function(label = "custom module") {
+    module(
+      label = label,
+      server = function(id, data) {
+        moduleServer(id, function(input, output, session) {
+          shiny::exportTestValues(
+            value = rnorm(1)
+          )
+        })
+      },
+      ui = function(id) {
+        ns <- NS(id)
+        h1("Module that exports a random value for testing")
+      }
+    )
+  }
+
+  app <- TealAppDriver$new(
+    data = simple_teal_data(),
+    modules = modules(
+      value_export_module(label = "Module 1"),
+      value_export_module(label = "Module 2")
+    )
+  )
+  app$wait_for_idle(timeout = default_idle_timeout)
+  test_exports <- app$get_values()$export
+
+  expect_equal(length(test_exports), 1)
+
+  app$navigate_teal_tab("Module 2")
+  test_exports <- app$get_values()$export
+
+  expect_equal(length(test_exports), 2)
+  app$stop()
+})
+
+
 testthat::test_that("e2e: filter panel only shows the data supplied using datanames", {
   app <- TealAppDriver$new(
     data = simple_teal_data(),
