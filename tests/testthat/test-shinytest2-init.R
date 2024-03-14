7d8846d50f13775614a4024b39f726bdd882be17
Marcin <133694481+m7pr@users.noreply.github.com>
2024-03-14 13:59:32 +0100

    Update tests/testthat/test-shinytest2-init.R
    Co-authored-by: André Veríssimo <211358+averissimo@users.noreply.github.com>
    Signed-off-by: Marcin <133694481+m7pr@users.noreply.github.com>

--- tests/testthat/test-shinytest2-init.R
+++ tests/testthat/test-shinytest2-init.R
@@ -74,7 +74,7 @@ testthat::test_that("e2e: teal app initializes with Show R Code modal", {
     "Copy to Clipboard"
   )
   testthat::expect_equal(
-    app$get_text("#shiny-modal > div > div > div.modal-body > div > button:nth-child(2)"),
+    app$get_text("#shiny-modal div.modal-footer button[data-dismiss]")
     "Dismiss"
   )
 
