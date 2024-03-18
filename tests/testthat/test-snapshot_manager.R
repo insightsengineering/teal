6b99ded58faaf07979e04611bf341f94fac4072c
Aleksander Chlebowski <aleksander.chlebowski@contractors.roche.com>
2024-03-18 15:45:18 +0100

    simplify unit test for snapshot manager

--- tests/testthat/test-snapshot_manager.R
+++ tests/testthat/test-snapshot_manager.R
@@ -14,12 +14,12 @@ testthat::test_that("snapshot manager holds initial state in history", {
   fd1 <- teal.slice::init_filtered_data(list(iris = list(dataset = iris)))
   fd2 <- teal.slice::init_filtered_data(list(iris = list(dataset = iris), mtcars = list(dataset = mtcars)))
   fd3 <- teal.slice::init_filtered_data(list(iris = list(dataset = iris), women = list(dataset = women)))
-  filtered_data_list <- list(m1 = fd1, m2 = fd2, m3 = fd3)
+  datasets_flat <- list(m1 = fd1, m2 = fd2, m3 = fd3)
 
   slices_global <- reactiveVal(shiny::isolate(filter))
 
   mapping_matrix <- reactive({
-    module_states <- lapply(filtered_data_list, function(x) x$get_filter_state())
+    module_states <- lapply(datasets_flat, function(x) x$get_filter_state())
     mapping_ragged <- lapply(module_states, function(x) vapply(x, `[[`, character(1L), "id"))
     all_names <- vapply(slices_global(), `[[`, character(1L), "id")
     mapping_smooth <- lapply(mapping_ragged, is.element, el = all_names)
@@ -32,7 +32,7 @@ testthat::test_that("snapshot manager holds initial state in history", {
       id = "test",
       slices_global = slices_global,
       mapping_matrix = mapping_matrix,
-      datasets = filtered_data_list
+      datasets = datasets_flat
     ),
     expr = {
       testthat::expect_true("Initial application state" %in% names(snapshot_history()))
