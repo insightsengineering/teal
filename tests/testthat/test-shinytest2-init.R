1890e0594494a2a25c26ceea1cb706d195b730f2
Marcin <133694481+m7pr@users.noreply.github.com>
2024-03-19 10:50:10 +0100

    503 shinytest2 for session info link in bottom of the page (#1145)
    Part of https://github.com/insightsengineering/coredev-tasks/issues/503
    
    ---------
    
    Signed-off-by: Marcin <133694481+m7pr@users.noreply.github.com>
    Co-authored-by: André Veríssimo <211358+averissimo@users.noreply.github.com>

--- tests/testthat/test-shinytest2-init.R
+++ tests/testthat/test-shinytest2-init.R
@@ -8,6 +8,62 @@ testthat::test_that("e2e: teal app initializes with no errors", {
   app$stop()
 })
 
+testthat::test_that("e2e: teal app initializes with sessionInfo modal", {
+  app <- TealAppDriver$new(
+    data = simple_teal_data(),
+    modules = example_module(label = "Example Module")
+  )
+  app$wait_for_idle(timeout = default_idle_timeout)
+
+  # Check if button exists.
+  button_selector <- "#teal-sessionInfo-button"
+  testthat::expect_equal(
+    app$get_text(button_selector),
+    "Session Info"
+  )
+
+  app$click(selector = button_selector)
+  app$wait_for_idle(timeout = default_idle_timeout)
+
+  # Check header and title content.
+  testthat::expect_equal(
+    app$get_text("#shiny-modal > div > div > div.modal-header > h4"),
+    "SessionInfo"
+  )
+
+  # There are two Copy buttons with similar id and the same label.
+  testthat::expect_setequal(
+    testthat::expect_length(
+      app$get_text(
+        "#shiny-modal [id^='teal-sessionInfo-copy_button']"
+      ),
+      2
+    ),
+    "Copy to Clipboard"
+  )
+  # There are two Dismiss buttons with similar id and the same label.
+  testthat::expect_setequal(
+    testthat::expect_length(
+      app$get_text("#shiny-modal button[data-dismiss]"),
+      2
+    ),
+    "Dismiss"
+  )
+
+  # Check session info output.
+  session_info <- app$get_text("#teal-sessionInfo-verbatim_content")
+
+  testthat::expect_match(session_info, "R version", fixed = TRUE)
+  testthat::expect_match(session_info, "attached base packages:", fixed = TRUE)
+  testthat::expect_match(session_info, "loaded via a namespace (and not attached):", fixed = TRUE)
+
+  testthat::expect_match(session_info, "shiny", fixed = TRUE)
+  testthat::expect_match(session_info, "teal.slice", fixed = TRUE)
+  testthat::expect_match(session_info, "teal.reporter", fixed = TRUE)
+
+  app$stop()
+})
+
 testthat::test_that("e2e: init creates UI containing specified title, favicon, header and footer", {
   app_title <- "Custom Teal App Title"
   app_favicon <- "https://raw.githubusercontent.com/insightsengineering/hex-stickers/main/PNG/teal.png"
