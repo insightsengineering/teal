320014e83e2017ffca5f910059c506c5ccf1e301
Aleksander Chlebowski <aleksander.chlebowski@contractors.roche.com>
2024-03-25 10:53:12 +0100

    update documentation for restoreValue

--- R/module_bookmark_manager.R
+++ R/module_bookmark_manager.R
@@ -208,14 +208,14 @@ bookmark_manager_srv <- function(id, slices_global, mapping_matrix, datasets, sn
 #'
 #' Get value from bookmark or return default.
 #'
-#' Server-side bookmarks can store not only inputs but also arbitrary values.
-#' These values are stored by `onBookmark` callbacks and restored by `onBookmarked` callbacks.
+#' Bookmarks can store not only inputs but also arbitrary values.
+#' These values are stored by `onBookmark` callbacks and restored by `onBookmarked` callbacks,
+#' and they are placed in the `values` environment in the `session$restoreContext` field.
 #' Using `teal_data_module` makes it impossible to run the callbacks
 #' because the app becomes ready before modules execute and callbacks are registered.
 #' In those cases the stored values can still be recovered from the `session` object directly.
 #'
-#' @section Use in modules:
-#' Variable names in the `values` environment are prefixed with module name space names,
+#' Note that variable names in the `values` environment are prefixed with module name space names,
 #' therefore, when using this function in modules, `value` must be run through the name space function.
 #'
 #' @param value (`character(1)`) name of value to restore
