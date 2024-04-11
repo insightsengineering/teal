ae35674de04aae204108209821d415fc3d55c726
André Veríssimo <211358+averissimo@users.noreply.github.com>
2024-04-11 14:54:35 +0200

    fix: remove duration from wait_for_idle(5000) it will reduce execution time

--- R/TealAppDriver.R
+++ R/TealAppDriver.R
@@ -73,7 +73,7 @@ TealAppDriver <- R6::R6Class( # nolint: object_name.
     #' @param ... arguments passed to parent [`shinytest2::AppDriver`] `click()` method.
     click = function(...) {
       super$click(...)
-      self$wait_for_idle(5000)
+      self$wait_for_idle()
     },
     #' @description
     #' Check if the app has shiny errors. This checks for global shiny errors.
