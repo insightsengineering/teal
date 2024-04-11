ccb070915f8dbe5ebf78ef48248bc6c9bab46af8
André Veríssimo <211358+averissimo@users.noreply.github.com>
2024-04-11 12:56:13 +0200

    feat: wait for idle by default after setting module input

--- R/TealAppDriver.R
+++ R/TealAppDriver.R
@@ -232,6 +232,8 @@ TealAppDriver <- R6::R6Class( # nolint: object_name.
         value,
         ...
       )
+      dots <- rlang::list2(...)
+      if (!isFALSE(dots[[wait]])) self$wait_for_idle() # Default behavior is to wait
       invisible(self)
     },
     #' @description
