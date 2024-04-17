6042f560e66a9317f2b6e905f54cc3101851ffd5
Dony Unardi <donyunardi@gmail.com>
2024-04-17 08:41:10 -0700

    fix logs

--- R/TealAppDriver.R
+++ R/TealAppDriver.R
@@ -250,8 +250,8 @@ TealAppDriver <- R6::R6Class( # nolint: object_name.
         )
       )
 
-      self$log_message(sprintf("Value for displayed_datasets_index: %s", toString(available_datasets)))
-      self$log_message(sprintf("Value for available_datasets: %s", toString(displayed_datasets_index)))
+      self$log_message(sprintf("Value for displayed_datasets_index: %s", toString(displayed_datasets_index)))
+      self$log_message(sprintf("Value for available_datasets: %s", toString(available_datasets)))
 
       available_datasets[displayed_datasets_index]
     },
