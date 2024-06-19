6cfcad68a6b3ec6e310a2838c4fdca2e6edfa23f
Aleksander Chlebowski <aleksander.chlebowski@sanofi.com>
2024-06-19 12:48:46 +0200

    apply review suggestions

--- inst/js/sidebar.js
+++ inst/js/sidebar.js
@@ -6,17 +6,21 @@ b/c in embedded apps it will throw errors that cause the function to exit early
 var filter_open = true;
 const hideSidebar = () => {
   $(".teal_secondary_col").css("display", "none");
-  $(".teal_primary_col").attr("class", "teal_primary_col col-sm-12");
-  $(".teal_primary_col").resize();
+  $(".teal_primary_col")
+    .removeClass("col-sm-9")
+    .addClass("col-sm-12");
+  $(".teal_primary_col").trigger("resize");
 };
 const showSidebar = () => {
-  $(".teal_primary_col").attr("class", "teal_primary_col col-sm-9");
+  $(".teal_primary_col")
+    .removeClass("col-sm-12")
+    .addClass("col-sm-9");
   setTimeout(
     () => {
       $(".teal_secondary_col").css("display", "block");
     },
     600);
-  $(".teal_primary_col").resize();
+  $(".teal_primary_col").trigger("resize");
 };
 const toggleFilterPanel = () => {
   if (filter_open && !$(".teal_secondary_col").is(':visible')) {
