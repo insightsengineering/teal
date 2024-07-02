f7fbc87d0b886635ff74ba366bb3a00ebe5e3504
Marcin <133694481+m7pr@users.noreply.github.com>
2024-07-02 15:03:52 +0200

    Update tests/testthat/test-utils.R
    Co-authored-by: Pawel Rucki <12943682+pawelru@users.noreply.github.com>
    Signed-off-by: Marcin <133694481+m7pr@users.noreply.github.com>

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -256,7 +256,7 @@ testthat::test_that("defunction recursively goes down a list", {
 testthat::test_that("create_renv_lockfile creates a lock file during the execution", {
   renv_file_name <- "teal_app.lock"
   withr::defer(file.remove(renv_file_name))
-  teal:::create_renv_lockfile(renv_file_name)
+  create_renv_lockfile(renv_file_name)
 
   testthat::expect_true(file.exists(renv_file_name))
 })
