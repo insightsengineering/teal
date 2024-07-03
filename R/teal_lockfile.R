6b1c0df0147d8374443c2f96ae399854cf29c0c3
m7pr <marcin.kosinski.mk1@roche.com>
2024-07-03 14:06:22 +0200

    expose env = NULL and unify documentation

--- R/teal_lockfile.R
+++ R/teal_lockfile.R
@@ -49,13 +49,13 @@ teal_lockfile <- function() {
         lockfile_path = lockfile_path,
         opts = options()
       ),
-      # print results to the console
+      # prints results to the console
       stdout = "",
       # renv setup is orchestrated by its special S3 objects: renv::settings, renv::config and renv::paths
-      #     `package` parameter include `renv` namespace inside the environment of `func = create_renv_lockfile` fun
-      package = "renv"
-      # default env = NULL # means that callr process will use environmental variables
-      #     from the main session (parent process)
+      #     `package` parameter include `renv` namespace inside the environment of `func = create_renv_lockfile`
+      package = "renv",
+      # callr process will use environmental variables from the main session (parent process)
+      env = NULL
     ),
     type = "message"
   )
