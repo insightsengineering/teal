7c6ec0b497da0a921e70f9b2b6d10521fd5a8a86
m7pr <marcin.kosinski.mk1@roche.com>
2024-07-24 11:41:07 +0200

    substitute |> with %>%

--- tests/testthat/test-module_nested_tabs.R
+++ tests/testthat/test-module_nested_tabs.R
@@ -300,7 +300,7 @@ testthat::test_that("srv_teal_module: teal_module gets data even if any transfor
       datasets()$set_filter_state(teal_slices(teal_slice("iris", "Species", selected = "virginica")))
       session$flushReact()
       out <- module_out()
-      testthat::expect_identical(out()[["iris"]], dplyr::filter(iris, Species == "virginica") |> head(6))
+      testthat::expect_identical(out()[["iris"]], dplyr::filter(iris, Species == "virginica") %>% head(6))
       testthat::expect_identical(out()[["mtcars"]], head(mtcars, 6))
     }
   )
@@ -497,8 +497,8 @@ testthat::test_that("srv_teal_module: teal_module receives transformed data with
       datasets()$set_filter_state(teal_slices(teal_slice("mtcars", "cyl", selected = "6")))
       session$flushReact()
       out <- module_out()
-      testthat::expect_identical(out()[["iris"]], dplyr::filter(iris, Species == "virginica") |> head(6))
-      testthat::expect_identical(out()[["mtcars"]], dplyr::filter(mtcars, cyl == 6) |> head(6))
+      testthat::expect_identical(out()[["iris"]], dplyr::filter(iris, Species == "virginica") %>% head(6))
+      testthat::expect_identical(out()[["mtcars"]], dplyr::filter(mtcars, cyl == 6) %>% head(6))
       hashes <- lapply(c("data3", "iris", "mtcars"), function(dataname) {
         sprintf(
           "stopifnot(rlang::hash(%s) == \"%s\")",
