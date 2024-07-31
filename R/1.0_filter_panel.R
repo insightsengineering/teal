497fbfe3d744560ab6da4c60ac2883e537f263aa
vedhav <vedhaviyash4@gmail.com>
2024-07-31 09:08:07 +0530

    fix: init filter panel with new ns

--- R/1.0_filter_panel.R
+++ R/1.0_filter_panel.R
@@ -28,16 +28,18 @@ ui_filter_panel <- function(id) {
 srv_filter_panel <- function(id, datasets, active_datanames, data_rv, is_active) {
   checkmate::assert_class(datasets, "reactive")
   moduleServer(id, function(input, output, session) {
+    index <- reactiveVal(1L)
     output$panel <- renderUI({
       req(inherits(datasets(), "FilteredData"))
       isolate({
         # render will be triggered only when FilteredData object changes (not when filters change)
         # technically it means that teal_data_module needs to be refreshed
         logger::log_debug("srv_filter_panel rendering filter panel.")
+        index(index() + 1L)
         filtered_data <- datasets()
-        filtered_data$srv_active("filters", active_datanames = active_datanames)
         # todo: make sure to bump the `teal.slice` version. Please use the branch `669_insertUI@main` in `teal.slice`.
-        filtered_data$ui_active(session$ns("filters"), active_datanames = active_datanames)
+        filtered_data$srv_active(sprintf("filters_%s", index()), active_datanames = active_datanames)
+        filtered_data$ui_active(session$ns(sprintf("filters_%s", index())), active_datanames = active_datanames)
       })
     })
 
