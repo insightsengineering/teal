4834f4eed658ba4f267900fbb4ba8e8e99a3d75f
go_gonzo <dawid.kaledkowski@gmail.com>
2024-08-20 08:36:47 +0200

    turnoff renv in tests

--- tests/testthat/setup-options.R
+++ tests/testthat/setup-options.R
@@ -1,9 +1,27 @@
+# we should't run lockfile process multiple times in tests as it starts background process which is not
+# possible to kill once run. It means that background R sessions are being cumulated
 withr::local_options(
-  list(
-    warnPartialMatchDollar = TRUE,
-    warnPartialMatchArgs = TRUE,
-    warnPartialMatchAttr = TRUE,
-    teal.renv.enable = FALSE
-  ),
+  list(teal.renv.enable = FALSE),
   .local_envir = testthat::teardown_env()
 )
+
+# `opts_partial_match_old` is left for exclusions due to partial matching in dependent packages (i.e. not fixable here)
+# it might happen that it is not used right now, but it is left for possible future use
+# use with: `withr::with_options(opts_partial_match_old, { ... })` inside the test
+opts_partial_match_old <- list(
+  warnPartialMatchDollar = getOption("warnPartialMatchDollar"),
+  warnPartialMatchArgs = getOption("warnPartialMatchArgs"),
+  warnPartialMatchAttr = getOption("warnPartialMatchAttr")
+)
+opts_partial_match_new <- list(
+  warnPartialMatchDollar = TRUE,
+  warnPartialMatchArgs = TRUE,
+  warnPartialMatchAttr = TRUE
+)
+
+if (isFALSE(getFromNamespace("on_cran", "testthat")()) && requireNamespace("withr", quietly = TRUE)) {
+  withr::local_options(
+    opts_partial_match_new,
+    .local_envir = testthat::teardown_env()
+  )
+}
