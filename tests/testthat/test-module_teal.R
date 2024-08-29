11a6c364f39363451863a5061adea53a2d78f08e
m7pr <marcin.kosinski.mk1@roche.com>
2024-08-29 12:02:29 +0200

    fix one test for 1304 branch

--- tests/testthat/test-module_teal.R
+++ tests/testthat/test-module_teal.R
@@ -364,7 +364,7 @@ testthat::describe("srv_teal teal_modules", {
     )
   })
 
-  testthat::it("are not called when the teal_data_module returns qenv.error", {
+  testthat::it("are called when teal_data_module returns qenv.error", {
     shiny::testServer(
       app = srv_teal,
       args = list(
@@ -384,9 +384,12 @@ testthat::describe("srv_teal teal_modules", {
       ),
       expr = {
         testthat::expect_null(modules_output$module_1())
-        testthat::expect_error(data_rv())
+        testthat::expect_identical(
+          ls(teal.code::get_env(data_rv())),
+          character(0)
+        )
         session$setInputs(`teal_modules-active_tab` = "module_1")
-        testthat::expect_null(modules_output$module_1())
+        testthat::expect_identical(modules_output$module_1(), 101L)
       }
     )
   })
