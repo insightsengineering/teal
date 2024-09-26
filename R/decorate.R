b74d1f605755ade2419e51bfa1aa9d5f1213af07
27856297+dependabot-preview[bot]@users.noreply.github.com <27856297+dependabot-preview[bot]@users.noreply.github.com>
2024-09-26 15:07:53 +0200

    WIP

---
+++ R/decorate.R
@@ -0,0 +1,37 @@
+#' Modify object in teal_data environment
+#' @export
+setGeneric(
+  "decorate_teal_data",
+  function(ui = function(id) NULL, server, expr) {
+    standardGeneric("decorate_teal_data")
+  }
+)
+
+setMethod(
+  "decorate_teal_data",
+  signature = list(server = "function"),
+  function(ui = function(id) NULL, server, expr) {
+    if (!missing(expr)) {
+      stop("expr argument ignored when ui and server are specified")
+    }
+    teal_transform_module(ui, server)
+  }
+)
+
+
+setMethod( # comment: this could be teal_transform_module method
+  "decorate_teal_data",
+  signature = list(expr = "language"),
+  function(ui = function(id) NULL, server, expr) {
+    decorate_teal_data(
+      server = function(id, data) {
+        moduleServer(id, function(input, output, session) {
+          reactive({
+            req(data())
+            data() |> eval_code(expr)
+          })
+        })
+      }
+    )
+  }
+)
