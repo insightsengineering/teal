6835c188942c9a9f0d0f63258cf60abc417f3b5a
27856297+dependabot-preview[bot]@users.noreply.github.com <27856297+dependabot-preview[bot]@users.noreply.github.com>
2024-09-27 10:25:55 +0200

    init

--- R/decorate.R
+++ R/decorate.R
@@ -9,6 +9,14 @@ setGeneric(
 
 setMethod(
   "decorate_teal_data",
+  signature = list(server = "missing", expr = "missing"),
+  function(ui = function(id) NULL, server, expr) {
+    teal_transform_module(ui, server = function(id, data) data)
+  }
+)
+
+setMethod(
+  "decorate_teal_data",
   signature = list(server = "function"),
   function(ui = function(id) NULL, server, expr) {
     if (!missing(expr)) {
@@ -18,6 +26,22 @@ setMethod(
   }
 )
 
+setMethod( # comment: this could be teal_transform_module method
+  "decorate_teal_data",
+  signature = list(expr = "language"),
+  function(ui = function(id) NULL, server, expr) {
+    decorate_teal_data(
+      server = function(id, data) {
+        moduleServer(id, function(input, output, session) {
+          reactive({
+            req(data())
+            data() |> eval_code(expr)
+          })
+        })
+      }
+    )
+  }
+)
 
 setMethod( # comment: this could be teal_transform_module method
   "decorate_teal_data",
