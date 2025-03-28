738dd69b2332702a3bd50e56e5d334b428e37e6f
André Veríssimo <211358+averissimo@users.noreply.github.com>
2025-03-28 12:08:41 +0000

    feat: stable factory creation

--- R/module_validate.R
+++ R/module_validate.R
@@ -1,12 +1,10 @@
 #' @keywords internal
 ui_validate_error <- function(id) {
-  ns <- NS(id)
-  # uiOutput(NS(id, ns("message")))
   tagList(
-    ui_module_validate_shinysilenterror(ns("validate_shinysilenterror")),
-    ui_module_validate_reactive(ns("validate_reactive")),
-    ui_module_validate_qenverror(ns("validate_qenverror")),
-    ui_module_validate_error(ns("validate_error"))
+    module_validate_shinysilenterror$ui(id),
+    module_validate_reactive$ui(id),
+    module_validate_teal_data$ui(id),
+    module_validate_error$ui(id)
   )
 }
 
@@ -15,10 +13,13 @@ srv_validate_error <- function(id, data, validate_shiny_silent_error) {
   checkmate::assert_string(id)
   checkmate::assert_flag(validate_shiny_silent_error)
   moduleServer(id, function(input, output, session) {
-    srv_module_validate_shinysilenterror(data)
-    srv_module_validate_reactive(data)
-    srv_module_validate_qenverror(data)
-    srv_module_validate_error(data)
+    # New
+    module_validate_shinysilenterror$server(data)
+    module_validate_reactive$server(data, types = c("simpleError", "teal_data"))
+    module_validate_teal_data$server(data)
+    module_validate_error$server(data)
+
+    # # Old
     # output$message <- renderUI({
     #   is_shiny_silent_error <- inherits(data(), "shiny.silent.error") && identical(data()$message, "")
     #   if (inherits(data(), "qenv.error")) {
@@ -52,6 +53,178 @@ srv_validate_error <- function(id, data, validate_shiny_silent_error) {
   })
 }
 
+
+# #######################################
+#
+#   _       _                        _
+#  (_)     | |                      | |
+#   _ _ __ | |_ ___ _ __ _ __   __ _| |
+#  | | '_ \| __/ _ \ '__| '_ \ / _` | |
+#  | | | | | ||  __/ |  | | | | (_| | |
+#  |_|_| |_|\__\___|_|  |_| |_|\__,_|_|
+#
+#
+#
+#  internal
+# ######################################
+
+#' Factory to build validate modules
+#'
+#' This function is used to create a module that validates the reactive data
+#' passed to it.
+#'
+#' Dynamically generation of an `ui` and `server` function that can be used
+#' internally in teal or in a teal module.
+#'
+#'
+#'
+#' @param module_id (`character(1)`) The module id.
+#' @param ... (`function`) 1 or more [`shiny::moduleServer()`] functions that
+#' return a [`shiny::reactive()`] with `TRUE` or a character string detailing
+#' the excpetion.
+#' It can be a named function, a character string or an anonymous function.
+#'
+#' @returns A list with `ui` and `server` functions with code generated from the
+#' arguments.
+#'
+#' @examples
+#'
+#' check_error <- function(x, skip_on_empty_message = TRUE) {
+#'   moduleServer("check_error", function(input, output, session) {
+#'     reactive({
+#'       if (inherits(x(), "error") && (!skip_on_empty_message || !identical(x()$message, ""))) {
+#'         c("Error detected", x()$message)
+#'       } else {
+#'         TRUE
+#'       }
+#'     })
+#'   })
+#' }
+#'
+#' module_validate_factory("validate_error", check_error)
+#'
+#' check_numeric <- function(x, skip = FALSE) {
+#'   moduleServer("check_numeric", function(input, output, session) {
+#'     reactive(if (inherits(x(), numeric) || skip) TRUE else "Error: is not numeric")
+#'   })
+#' }
+#'
+#' module_validate_factory("validate_error", check_error, check_numeric)
+#' @export
+module_validate_factory <- function(module_id, ...) {
+  dots <- rlang::list2(...)
+  checkmate::check_list(dots, min.len = 1)
+
+  fun_names <- match.call(expand.dots = FALSE)[["..."]]
+
+  check_calls <- lapply(
+    seq_len(length(dots)),
+    function(fun_ix) {
+      fun_name <- fun_names[[fun_ix]]
+      fun_formals <- formals(dots[[fun_ix]])
+
+      substitute(
+        expr,
+        list(
+          expr = substitute(
+            collection <- append(collection, check_call),
+            list(check_call = rlang::call2(fun_name, !!!lapply(names(fun_formals), as.name)))
+          )
+        )
+      )
+    }
+  )
+
+  top_level_formals <- Reduce(
+    function(u, v) {
+      new_formals <- formals(v)
+      common <- intersect(names(new_formals), names(u))
+      vapply(common, function(x_name) {
+        if (identical(new_formals[[x_name]], u[[x_name]])) {
+          TRUE
+        } else {
+          stop("Arguments for check function have conflicting definitions (different defaults)")
+        }
+      }, FUN.VALUE = logical(1L))
+      append(u, new_formals[setdiff(names(new_formals), names(u))])
+    },
+    init = list(),
+    x = dots
+  )
+
+  template_str = "check_calls"
+
+  # Template moduleServer that supports multiple checks
+  module_server_body <- substitute({
+    collection <- list()
+    check_calls
+
+    validate_r <- reactive({
+      message_collection <- Reduce(
+        function(u, v) if (isTRUE(v())) u else c(u, v()),
+        x = collection,
+        init = c()
+      )
+
+      validate(need(length(message_collection) == 0, message_collection))
+      TRUE
+    })
+
+    output$errors <- renderUI({
+      validate_r()
+      NULL
+    })
+
+    x
+  }, list(check_calls == as.name(template_str)))
+
+  new_body_list <- .substitute_template(template_str, module_server_body, check_calls)
+
+  server_body <- substitute({
+    moduleServer(module_id, function(input, output, session) server_body)
+  }, list(module_id = module_id, server_body = new_body_list))
+
+  new_server_fun = function() TRUE
+  formals(new_server_fun) <- top_level_formals
+  body(new_server_fun) <- server_body
+
+  new_ui_fun <- function(id) TRUE
+  body(new_ui_fun) <- substitute({
+    ns <- NS(NS(id, module_id)) # id is defined at factory creation level
+    uiOutput(ns("errors"))
+  }, list(module_id = module_id))
+
+  list(ui = new_ui_fun, server = new_server_fun)
+}
+
+#' Custom substitute function that injects multiple lines to an expression
+#'
+#' It must contain the `template_str` on the first level of the expression.
+#'
+#' @param template_str (`character(1)`) The call in the expression to be replaced.
+#' @param module_server_body (`expression`) Any syntactically valid R expression.
+#' @param check_calls (`list`) A list of expressions to be injected.
+#'
+#' @returns An expression with the `template_str` replaced by the `check_calls`.
+#'
+#' @keywords internal
+.substitute_template <- function(template_str, module_server_body, check_calls) {
+  # Create server body with expressions for multiple checks
+  # note: using substitute directly will add curly braces around body
+  # todo: discuss this approach vs. having curly braces
+  body_list <- as.list(module_server_body)[-1]
+  ix <- which(body_list == as.name(template_str))
+
+  as.call(
+    c(
+      quote(`{`),
+      body_list[seq(1, ix - 1)],
+      check_calls,
+      body_list[seq(ix + 1, length(body_list))]
+    )
+  )
+}
+
 # #############################################################################
 #
 #              _ _     _       _                             _   _
@@ -66,29 +239,18 @@ srv_validate_error <- function(id, data, validate_shiny_silent_error) {
 #  validate reactive
 # ############################################################################
 
-srv_module_validate_reactive <- function(x, types = character(0L), null.ok = FALSE) {
-  moduleServer("validate_reactive", function(input, output, session) {
-    collection <- list()
-    collection <- append(collection, srv_module_check_reactive(x, types = types, null.ok = null.ok))
-
-    validate_r <- reactive({
-      message_collection <- clean_collection(collection)
-      validate(need(length(message_collection) == 0, message_collection))
-      TRUE
-    })
-
-    output$errors <- renderUI({
-      validate_r()
-      NULL
-    })
-
-
-    x
-  })
-}
-
-ui_module_validate_reactive <- function(id) uiOutput(NS(id, "errors"))
-
+#' Validate if an argument is a reactive
+#'
+#' @param x (`reactive`) A reactive value.
+#' @param types (`character`) A character vector with the types that the reactive.
+#' @param null.ok (`logical`) If `TRUE`, the `x` argument can be `NULL`.
+#'
+#' @name module_validate_reactive
+#' @seealso [module_validate_factory()]
+#'
+#' @returns A module that validates the reactive value.
+#'
+#' @export
 srv_module_check_reactive <- function(x, types = character(0L), null.ok = FALSE) {
   reactive_message <- check_reactive(x, null.ok = null.ok)
   moduleServer("check_reactive", function(input, output, session) {
@@ -105,11 +267,25 @@ srv_module_check_reactive <- function(x, types = character(0L), null.ok = FALSE)
           TRUE
         }
       } else {
-        reactive_message
+        paste0("NEW: ", reactive_message)
       }
     })
   })
 }
+
+#' @rdname module_validate_reactive
+#' @param id (`character`) The module id.
+#' @usage module_validate_reactive$server(x, types = character(0L), null.ok = FALSE)
+#' module_validate_reactive$ui(id)
+#' @examples
+#' module_validate_reactive$ui("validate_reactive")
+#'
+#' # Show the generated server function
+#' print(module_validate_reactive$server)
+#' @export
+module_validate_reactive <- module_validate_factory(
+  "validate_reactive", srv_module_check_reactive
+)
 
 # ###########################################################################
 #
@@ -125,33 +301,21 @@ srv_module_check_reactive <- function(x, types = character(0L), null.ok = FALSE)
 #  shinysilenterror
 # ##########################################################################
 
-srv_module_validate_shinysilenterror <- function(x) {
-  moduleServer("validate_shinysilenterror", function(input, output, session) {
-    collection <- list()
-    collection <- append(collection, srv_module_check_shinysilenterror(x))
-
-    validate_r <- reactive({
-      message_collection <- clean_collection(collection)
-      validate(need(length(message_collection) == 0, message_collection))
-      TRUE
-    })
-
-    output$errors <- renderUI({
-      validate_r()
-      NULL
-    })
-
-    x
-  })
-}
-
-ui_module_validate_shinysilenterror <- function(id) uiOutput(NS(id, "errors"))
-
+#' Validate if an argument contains a `shiny.silent.error`
+#'
+#' @param x (`reactive`) A reactive value.
+#'
+#' @name module_validate_shinysilenterror
+#' @seealso [module_validate_factory()]
+#'
+#' @returns A module that validates the reactive value.
+#'
+#' @export
 srv_module_check_shinysilenterror <- function(x) {
   moduleServer("check_shinysilenterror", function(input, output, session) {
     reactive({
       if (inherits(x(), "shiny.silent.error") && identical(x()$message, "")) {
-        "Shiny silent error was raised"
+        "NEW: Shiny silent error was raised"
       } else {
         TRUE
       }
@@ -159,33 +323,41 @@ srv_module_check_shinysilenterror <- function(x) {
   })
 }
 
-# ###############################################################
+#' @rdname module_validate_shinysilenterror
+#' @param id (`character`) The module id.
+#' @usage module_validate_shinysilenterror$ui(id)
+#' module_validate_shinysilenterror$server(x)
+#' @examples
+#' module_validate_shinysilenterror$ui("validate_reactive")
+#'
+#' # Show the generated server function
+#' print(module_validate_shinysilenterror$server)
+#' @export
+module_validate_shinysilenterror <- module_validate_factory(
+  "validate_shinysilenterror", srv_module_check_shinysilenterror
+)
+
+# ###############################################################################
 #
-#              _ _     _       _
-#             | (_)   | |     | |
-#  __   ____ _| |_  __| | __ _| |_ ___    __ _  ___ _ ____   __
-#  \ \ / / _` | | |/ _` |/ _` | __/ _ \  / _` |/ _ \ '_ \ \ / /
-#   \ V / (_| | | | (_| | (_| | ||  __/ | (_| |  __/ | | \ V /
-#    \_/ \__,_|_|_|\__,_|\__,_|\__\___|  \__, |\___|_| |_|\_/
-#                                           | |
-#                                           |_|
+#              _ _     _       _         _             _      _       _
+#             | (_)   | |     | |       | |           | |    | |     | |
+#  __   ____ _| |_  __| | __ _| |_ ___  | |_ ___  __ _| |  __| | __ _| |_ __ _
+#  \ \ / / _` | | |/ _` |/ _` | __/ _ \ | __/ _ \/ _` | | / _` |/ _` | __/ _` |
+#   \ V / (_| | | | (_| | (_| | ||  __/ | ||  __/ (_| | || (_| | (_| | || (_| |
+#    \_/ \__,_|_|_|\__,_|\__,_|\__\___|  \__\___|\__,_|_| \__,_|\__,_|\__\__,_|
+#                                                     ______
+#                                                    |______|
 #
-#  validate qenv
-# ##############################################################
+#  validate teal_data
+# ##############################################################################
 
-srv_module_validate_qenverror <- function(x) {
-  srv_module_validate_generic("validate_qenverror", srv_module_check_qenverror, x)
-}
-
-ui_module_validate_qenverror <- function(id) uiOutput(NS(id, "errors"))
-
-srv_module_check_qenverror <- function(x) {
-  moduleServer("check_qenverror", function(input, output, session) {
+srv_module_check_teal_data <- function(x) {
+  moduleServer("check_teal_data", function(input, output, session) {
 
     reactive({
       if (inherits(x(), "qenv.error")) {
         c(
-          "Error when executing the `data` module:",
+          "NEW: Error when executing the `data` module:",
           cli::ansi_strip(x()$message),
           "",
           "Check your inputs or contact app developer if error persists."
@@ -197,23 +369,30 @@ srv_module_check_qenverror <- function(x) {
   })
 }
 
+module_validate_teal_data <- module_validate_factory(
+  "validate_teal_data", srv_module_check_teal_data
+)
 
+# ##################################################################
 #
-
-srv_module_validate_error <- function(x) {
-  srv_module_validate_generic("validate_error", srv_module_check_error, x)
-}
-
-ui_module_validate_error <- function(id) {
-  uiOutput(NS(id, "errors"))
-}
+#              _ _     _       _
+#             | (_)   | |     | |
+#  __   ____ _| |_  __| | __ _| |_ ___    ___ _ __ _ __ ___  _ __
+#  \ \ / / _` | | |/ _` |/ _` | __/ _ \  / _ \ '__| '__/ _ \| '__|
+#   \ V / (_| | | | (_| | (_| | ||  __/ |  __/ |  | | | (_) | |
+#    \_/ \__,_|_|_|\__,_|\__,_|\__\___|  \___|_|  |_|  \___/|_|
+#
+#
+#
+#  validate error
+# #################################################################
 
 srv_module_check_error <- function(x) {
   moduleServer("check_error", function(input, output, session) {
 
     reactive({
       if (inherits(x(), "error") && !inherits(x(), c("qenv.error", "shiny.silent.error"))) {
-        c("Error detected", x()$message)
+        c("NEW: Error detected", x()$message)
       } else {
         TRUE
       }
@@ -221,15 +400,42 @@ srv_module_check_error <- function(x) {
   })
 }
 
-# Aux
+module_validate_error <- module_validate_factory(
+  "validate_error", srv_module_check_error
+)
 
-srv_module_validate_generic <- function(id, fun, x, ...) {
-  moduleServer(id, function(input, output, session) {
+# ##########################################
+#
+#                                    ___
+#                                   |__ \
+#   _ __ ___ _ __ ___   _____   _____  ) |
+#  | '__/ _ \ '_ ` _ \ / _ \ \ / / _ \/ /
+#  | | |  __/ | | | | | (_) \ V /  __/_|
+#  |_|  \___|_| |_| |_|\___/ \_/ \___(_)
+#
+#
+#
+#  todo: remove?
+# #########################################
+
+.substitute_template_curly <- function(template_str, module_server_body, check_calls) {
+  call_inject <- if (length(check_calls) > 1) {
+    as.call(c(quote(`{`), check_calls))
+  } else {
+    as.call(check_calls[[1]])
+  }
+
+  vv <- substitute({
     collection <- list()
-    collection <- append(collection, fun(x, ...))
+    check_calls
 
     validate_r <- reactive({
-      message_collection <- clean_collection(collection)
+      message_collection <- Reduce(
+        function(u, v) if (isTRUE(v())) u else c(u, v()),
+        x = collection,
+        init = c()
+      )
+
       validate(need(length(message_collection) == 0, message_collection))
       TRUE
     })
@@ -240,20 +446,74 @@ srv_module_validate_generic <- function(id, fun, x, ...) {
     })
 
     x
-  })
+  }, list(check_calls = call_inject))
 }
 
-clean_collection <- function(collection) {
-  Reduce(
-    function(u, v) {
-      el <- v()
-      if (isTRUE(el)) {
-        u
-      } else {
-        c(u, el)
-      }
+module_validate_factory_single <- function(module_id, check_fun) {
+  fun_name <- if (is.character(check_fun)) check_fun else deparse(substitute(check_fun))
+  fun_formals <- formals(check_fun)
+
+  server_body <- substitute(
+    {
+      moduleServer(module_id, function(input, output, session) {
+        collection <- list()
+        # todo: start with a req() of first argument
+        collection <- append(collection, check_call)
+
+        validate_r <- reactive({
+          message_collection <- Reduce(
+            function(u, v) if (isTRUE(v())) u else c(u, v()),
+            x = collection,
+            init = c()
+          )
+
+          validate(need(length(message_collection) == 0, message_collection))
+          TRUE
+        })
+
+        output$errors <- renderUI({
+          validate_r()
+          NULL
+        })
+
+        x
+      })
     },
-    x = collection,
-    init = c()
+    list(
+      # Generates call with exact formals of check function
+      check_call = rlang::call2(fun_name, !!!lapply(names(fun_formals), as.name)),
+      module_id = module_id
+    )
+  )
+
+  new_server_fun = function() TRUE
+  formals(new_server_fun) <- formals(check_fun)
+  body(new_server_fun) <- server_body
+
+  new_ui_fun <- function(id) TRUE
+  body(new_ui_fun) <- substitute(
+    {
+      uiOutput(NS(NS(id, module_id), "errors"))
+    },
+    list(module_id = module_id)
+  )
+
+  list(
+    ui = new_ui_fun,
+    server = new_server_fun
   )
 }
+
+#########################################################################
+#
+#                  _          __                                    ___
+#                 | |        / _|                                  |__ \
+#    ___ _ __   __| |   ___ | |_   _ __ ___ _ __ ___   _____   _____  ) |
+#   / _ \ '_ \ / _` |  / _ \|  _| | '__/ _ \ '_ ` _ \ / _ \ \ / / _ \/ /
+#  |  __/ | | | (_| | | (_) | |   | | |  __/ | | | | | (_) \ V /  __/_|
+#   \___|_| |_|\__,_|  \___/|_|   |_|  \___|_| |_| |_|\___/ \_/ \___(_)
+#
+#
+#
+#  end of remove?
+# ########################################################################
