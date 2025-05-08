0372479e6dc0483575cb2476389f5004c3409dd4
m7pr <marcin.kosinski.mk1@roche.com>
2025-05-08 14:28:38 +0200

    documentation and checks for modify_reactive_output

--- R/modify_reactive_output.R
+++ R/modify_reactive_output.R
@@ -1,11 +1,46 @@
-# This function is just here, so it is easier to understand what's happening in
-# modify_reactive_output
-# We want to have a match in parameter names in new_server and original_server
-# assuming we don't know names of parameters in original_server.
-# modify_reactive_output is the final implementation that shows how to do it without knowing parameters.
+#' Modify Reactive Outputs of a `teal` Module
+#'
+#' @description
+#' This function takes a `teal_module` object and allows modification of its
+#' server's reactive outputs. It wraps the original module's server,
+#' applying specified functions to one or more of the outputs returned by the
+#' original server.
+#'
+#' The original server logic is called within a new server function.
+#' The transformations are applied to the outputs of the original server
+#' before they are returned by the new server.
+#'
+#' @param teal_module (`teal_module`)
+#' @param ... Named arguments where each name corresponds to an output element
+#'   (e.g., a reactive expression or a static value) returned by the
+#'   `teal_module`'s server function. The value for each named argument must be a
+#'   function that takes the original output's value as its single argument and
+#'   returns the modified value. If the original output is a reactive, its
+#'   resolved value is passed to the modifying function, and the modifying
+#'   function's result is wrapped in a new reactive.
+#'
+#' @return A new `teal_module` object with the modified server logic and a UI
+#'   that internally namespaces the original UI. The new module will have the
+#'   same label, datanames, server_args, ui_args, and transformators as the
+#'   original `teal_module`.
+#'
+#' @export
 modify_reactive_output <- function(teal_module, ...) {
   checkmate::assert_class(teal_module, "teal_module")
   output_funs <- list(...)
+
+  if (length(output_funs) == 0) {
+    warning("No output transformations specified for modify_reactive_output. Returning original module.")
+    return(teal_module)
+  }
+  if (is.null(names(output_funs)) || any(names(output_funs) == "" | duplicated(names(output_funs)))) {
+    stop("All transformations supplied to modify_reactive_output must be uniquely named.")
+  }
+  are_functions <- vapply(output_funs, is.function, logical(1))
+  if (!all(are_functions)) {
+    stop("All transformations supplied to modify_reactive_output must be functions.")
+  }
+
   original_server <- teal_module$server
   original_ui <- teal_module$ui
   new_ui <- function(id, ...) {
