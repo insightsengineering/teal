e6c40a9d0742d84abb798db8c601b58c7231b3ba
André Veríssimo <211358+averissimo@users.noreply.github.com>
2025-07-15 10:12:23 +0200

    e2e: bookmark_manager passes

--- tests/testthat/test-shinytest2-module_bookmark_manager.R
+++ tests/testthat/test-shinytest2-module_bookmark_manager.R
@@ -2,7 +2,6 @@ testthat::skip_if_not_installed("shinytest2")
 testthat::skip_if_not_installed("rvest")
 
 testthat::test_that("bookmark_manager_button is not rendered by default", {
-  testthat::skip("chromium")
   skip_if_too_deep(5)
   app <- TealAppDriver$new(
     data = simple_teal_data(),
@@ -15,9 +14,7 @@ testthat::test_that("bookmark_manager_button is not rendered by default", {
   )
 })
 
-
 testthat::test_that("bookmark_manager_button is not rendered when enableBookmarking = 'url'", {
-  testthat::skip("chromium")
   skip_if_too_deep(5)
   app <- TealAppDriver$new(
     data = simple_teal_data(),
@@ -25,14 +22,10 @@ testthat::test_that("bookmark_manager_button is not rendered when enableBookmark
     options = list(shiny.bookmarkStore = "url")
   )
   on.exit(app$stop())
-  testthat::expect_null(
-    app$get_html(".bookmark_manager_button")
-  )
+  testthat::expect_null(app$get_html(".bookmark_manager_button"))
 })
 
-
 testthat::test_that("bookmark_manager_button is rendered when enableBookmarking = 'server'", {
-  testthat::skip("chromium")
   skip_if_too_deep(5)
   app <- TealAppDriver$new(
     data = simple_teal_data(),
@@ -40,11 +33,10 @@ testthat::test_that("bookmark_manager_button is rendered when enableBookmarking 
     options = list(shiny.bookmarkStore = "server")
   )
   on.exit(app$stop())
-  testthat::expect_true(!is.null(app$get_html(".bookmark_manager_button")))
+  testthat::expect_false(is.null(app$get_html(".bookmark_manager_button")))
 })
 
 testthat::test_that("bookmark_manager_button shows modal with url containing state_id when clicked", {
-  testthat::skip("chromium")
   skip_if_too_deep(5)
   app <- TealAppDriver$new(
     data = simple_teal_data(),
@@ -56,6 +48,6 @@ testthat::test_that("bookmark_manager_button shows modal with url containing sta
 
   testthat::expect_match(
     rvest::html_text(app$get_html_rvest("div[id$=bookmark_modal] pre")),
-    "_state_id_"
+    "_state_id_=[a-zA-Z0-9]+",
   )
 })
