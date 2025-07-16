d2f020c5be6600632cbb607aebd311b4929de6a1
André Veríssimo <211358+averissimo@users.noreply.github.com>
2025-07-16 12:35:35 +0200

    chore: standardize with app() and use same selector when looking for null/not null

--- tests/testthat/test-shinytest2-module_bookmark_manager.R
+++ tests/testthat/test-shinytest2-module_bookmark_manager.R
@@ -1,39 +1,40 @@
 testthat::skip_if_not_installed("shinytest2")
 testthat::skip_if_not_installed("rvest")
 
-testthat::test_that("bookmark_manager_button is not rendered by default", {
-  skip_if_too_deep(5)
-  app <- TealAppDriver$new(
-    data = simple_teal_data(),
-    modules = example_module(label = "Example Module"),
-    options = list()
-  )
-  on.exit(app$stop())
-  testthat::expect_null(
-    app$get_html(".bookmark_manager_button")
-  )
-})
+testthat::describe("bookmark_manager_button is", {
+  bookmark_manager_selector <- ".bookmark_manager_button"
+  it("not rendered by default", {
+    skip_if_too_deep(5)
+    app <- TealAppDriver$new(
+      data = simple_teal_data(),
+      modules = example_module(label = "Example Module"),
+      options = list()
+    )
+    testthat::expect_null(app$get_html(bookmark_manager_selector))
+    app$stop()
+  })
 
-testthat::test_that("bookmark_manager_button is not rendered when enableBookmarking = 'url'", {
-  skip_if_too_deep(5)
-  app <- TealAppDriver$new(
-    data = simple_teal_data(),
-    modules = example_module(label = "Example Module"),
-    options = list(shiny.bookmarkStore = "url")
-  )
-  on.exit(app$stop())
-  testthat::expect_null(app$get_html(".bookmark_manager_button"))
-})
+  it("not rendered when enableBookmarking = 'url'", {
+    skip_if_too_deep(5)
+    app <- TealAppDriver$new(
+      data = simple_teal_data(),
+      modules = example_module(label = "Example Module"),
+      options = list(shiny.bookmarkStore = "url")
+    )
+    testthat::expect_null(app$get_html(bookmark_manager_selector))
+    app$stop()
+  })
 
-testthat::test_that("bookmark_manager_button is rendered when enableBookmarking = 'server'", {
-  skip_if_too_deep(5)
-  app <- TealAppDriver$new(
-    data = simple_teal_data(),
-    modules = example_module(label = "Example Module"),
-    options = list(shiny.bookmarkStore = "server")
-  )
-  on.exit(app$stop())
-  testthat::expect_type(app$get_html(".bookmark_manager_button"), "character")
+  it("rendered when enableBookmarking = 'server'", {
+    skip_if_too_deep(5)
+    app <- TealAppDriver$new(
+      data = simple_teal_data(),
+      modules = example_module(label = "Example Module"),
+      options = list(shiny.bookmarkStore = "server")
+    )
+    testthat::expect_type(app$get_html(bookmark_manager_selector), "character")
+    app$stop()
+  })
 })
 
 testthat::test_that("bookmark_manager_button shows modal with url containing state_id when clicked", {
@@ -50,4 +51,5 @@ testthat::test_that("bookmark_manager_button shows modal with url containing sta
     rvest::html_text(app$get_html_rvest("div[id$=bookmark_modal] pre")),
     "_state_id_=[a-zA-Z0-9]+",
   )
+  app$stop()
 })
