ced7fe76c8de6d74c22f4798836a2d3bf524d32e
Vedha Viyash <49812166+vedhav@users.noreply.github.com>
2025-08-01 16:18:03 +0530

    Update inst/js/busy-disable.js
    Signed-off-by: Vedha Viyash <49812166+vedhav@users.noreply.github.com>

--- inst/js/busy-disable.js
+++ inst/js/busy-disable.js
@@ -1,3 +1,5 @@
+// Primarily added to make sure the user does not open multiple modals when shiny is busy.
+// https://github.com/rstudio/shiny/issues/4261
 $(document).on("shiny:busy", function () {
   $(".teal-busy-disable").prop("disabled", true);
 });
