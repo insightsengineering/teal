a777dd1fb4a86f7950b45d45569531504de7683f
Dawid Kałędkowski <dawid.kaledkowski@gmail.com>
2025-08-13 16:10:31 +0200

    obtain a named list from environment (#1576)
    On teal.code@main `get_var` is hard-deprecated. Here is the easier way
    to obtain qenv objects as a list

--- R/module_init_data.R
+++ R/module_init_data.R
@@ -95,15 +95,9 @@ srv_init_data <- function(id, data) {
     c(
       list(code = trimws(c(teal.code::get_code(data), hashes), which = "right")),
       list(join_keys = teal.data::join_keys(data)),
-      sapply(
-        names(data),
-        teal.code::get_var,
-        object = data,
-        simplify = FALSE
-      )
+      as.list(data, all.names = TRUE)
     )
   )
-
   tdata@verified <- data@verified
   tdata
 }
