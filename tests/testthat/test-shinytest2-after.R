eaf3fdfe7ade9441dac7635376df09c18253e95e
Lluís Revilla <lluis.revilla_sancho@roche.com>
2025-09-24 10:16:56 +0200

    Close apps and add more tests for different patterns

--- tests/testthat/test-shinytest2-after.R
+++ tests/testthat/test-shinytest2-after.R
@@ -16,4 +16,76 @@ testthat::test_that("e2e: after adds wrapped to ids of modules", {
   expect_true(all(grep("wrapped", setdiff(names(after_ids$input), names(before_ids$input)), fixed = TRUE)))
 
   app$stop()
+  app_after$stop()
+})
+
+testthat::test_that("e2e: after modifies nested modules", {
+  skip_if_too_deep(5)
+  nested_modules <- modules(
+    label = "Nested modules",
+    example_module(label = "Module 1"),
+    example_module(label = "Module 2")
+  )
+  app <- TealAppDriver$new(
+    data = simple_teal_data(),
+    modules = nested_modules
+  )
+
+  app_after <- TealAppDriver$new(
+    data = simple_teal_data(),
+    modules = after(nested_modules)
+  )
+
+  before_ids <- app$get_values(input = TRUE, output = TRUE)
+  after_ids <- app_after$get_values(input = TRUE, output = TRUE)
+
+  new_input_ids <- setdiff(names(after_ids$input), names(before_ids$input))
+  module_1 <- grepl("module_1-module-wrapped", new_input_ids, fixed = TRUE)
+  module_2 <- grepl("module_2-module-wrapped", new_input_ids, fixed = TRUE)
+
+  expect_true(any(module_1), label = "Is 'Module 1' modified?")
+  expect_true(any(module_2), label = "Is 'Module 2' modified?")
+  expect_true(all(module_1 | module_2), label = "Are all modules modified?")
+
+  app$stop()
+  app_after$stop()
+})
+
+
+
+testthat::test_that("e2e: after modifies arbitrary modules structure", {
+  skip_if_too_deep(5)
+  nested_modules <- modules(
+    label = "Nested modules",
+    example_module(label = "Module 0"),
+    modules(label = "Nested",
+            example_module(label = "Module 1"),
+            example_module(label = "Module 2")
+            )
+  )
+  app <- TealAppDriver$new(
+    data = simple_teal_data(),
+    modules = nested_modules
+  )
+
+  app_after <- TealAppDriver$new(
+    data = simple_teal_data(),
+    modules = after(nested_modules)
+  )
+
+  before_ids <- app$get_values(input = TRUE, output = TRUE)
+  after_ids <- app_after$get_values(input = TRUE, output = TRUE)
+
+  new_input_ids <- setdiff(names(after_ids$input), names(before_ids$input))
+  module_0 <- grepl("module_0-module-wrapped", new_input_ids, fixed = TRUE)
+  module_1 <- grepl("module_1-module-wrapped", new_input_ids, fixed = TRUE)
+  module_2 <- grepl("module_2-module-wrapped", new_input_ids, fixed = TRUE)
+
+  expect_true(any(module_0), label = "Is 'Module 0' modified?")
+  expect_true(any(module_1), label = "Is 'Module 1' modified?")
+  expect_true(any(module_2), label = "Is 'Module 2' modified?")
+  expect_true(all(module_0 | module_1 | module_2), label = "Are all modules modified?")
+
+  app$stop()
+  app_after$stop()
 })
