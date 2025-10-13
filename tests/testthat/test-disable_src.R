a834926598283bf0437d1d4864db41000ae4548f
Lluís Revilla <lluis.revilla_sancho@roche.com>
2025-10-13 15:03:11 +0200

    Convert to test_that clauses

--- tests/testthat/test-disable_src.R
+++ tests/testthat/test-disable_src.R
@@ -1,24 +1,22 @@
-testthat::describe("srv_teal teal_modules disable_src", {
-  testthat::it("doesn't break module", {
-    shiny::testServer(
-      app = srv_teal,
-      args = list(
-        id = "test",
-        data = teal.data::teal_data(iris = iris),
-        modules = modules(example_module()) |> disable_src()
-      ),
-      expr = NULL
-    )
-  })
-  testthat::it("doesn't break multiple modules", {
-    shiny::testServer(
-      app = srv_teal,
-      args = list(
-        id = "test",
-        data = teal.data::teal_data(iris = iris),
-        modules = modules(example_module(), example_module()) |> disable_src()
-      ),
-      expr = NULL
-    )
-  })
+testthat::test_that("disable_src() doesn't break modules", {
+  shiny::testServer(
+    app = srv_teal,
+    args = list(
+      id = "test",
+      data = teal.data::teal_data(iris = iris),
+      modules = modules(example_module()) |> disable_src()
+    ),
+    expr = NULL
+  )
+})
+testthat::test_that("disable_src() doesn't break multiple modules", {
+  shiny::testServer(
+    app = srv_teal,
+    args = list(
+      id = "test",
+      data = teal.data::teal_data(iris = iris),
+      modules = modules(example_module(), example_module()) |> disable_src()
+    ),
+    expr = NULL
+  )
 })
--- tests/testthat/test-shinytest2-disable_report.R
+++ tests/testthat/test-shinytest2-disable_report.R
@@ -1,73 +1,70 @@
-testthat::describe("srv_teal teal_modules disable_report", {
+testthat::test_that("Report button is active on a module", {
+  testthat::skip_if_not_installed("shinytest2")
+  skip_if_too_deep(5)
+  app <- TealAppDriver$new(
+    data = simple_teal_data(),
+    modules = example_module(label = "m1")
+  )
 
-  testthat::it("Button is active on a module", {
-    testthat::skip_if_not_installed("shinytest2")
-    skip_if_too_deep(5)
-    app <- TealAppDriver$new(
-      data = simple_teal_data(),
-      modules = example_module(label = "m1")
+
+  expect_true(endsWith(app$get_attr(
+    selector = "#teal-teal_modules-nav-m1-add_reporter_wrapper-reporter_add-add_report_card_button",
+    attribute = "class"
+  ), "shiny-bound-input"))
+
+  app$stop()
+})
+testthat::test_that("Report button is disable on a module", {
+  testthat::skip_if_not_installed("shinytest2")
+  skip_if_too_deep(5)
+  app <- TealAppDriver$new(
+    data = simple_teal_data(),
+    modules = example_module(label = "m1") |> disable_report()
+  )
+
+
+  expect_true(endsWith(app$get_attr(
+    selector = "#teal-teal_modules-nav-m1-add_reporter_wrapper-reporter_add-add_report_card_button",
+    attribute = "class"
+  ), "shiny-bound-input disabled"))
+
+  app$stop()
+})
+
+testthat::it("Report button is active on nested module", {
+  testthat::skip_if_not_installed("shinytest2")
+  skip_if_too_deep(5)
+  app <- TealAppDriver$new(
+    data = simple_teal_data(),
+    modules = modules(
+      example_module(label = "m1"),
+      example_module(label = "m2")
     )
+  )
 
+  expect_true(endsWith(app$get_attr(
+    selector = "#teal-teal_modules-nav-m1-add_reporter_wrapper-reporter_add-add_report_card_button",
+    attribute = "class"
+  ), "shiny-bound-input"))
 
-    expect_true(endsWith(app$get_attr(
-      selector = "#teal-teal_modules-nav-m1-add_reporter_wrapper-reporter_add-add_report_card_button",
-      attribute = "class"
-    ), "shiny-bound-input"))
+  app$stop()
+})
 
-    app$stop()
-  })
-  testthat::it("Button is disable on a module", {
-    testthat::skip_if_not_installed("shinytest2")
-    skip_if_too_deep(5)
-    app <- TealAppDriver$new(
-      data = simple_teal_data(),
-      modules = example_module(label = "m1") |> disable_report()
-    )
+testthat::it("Report button is disabled on nested modules", {
+  testthat::skip_if_not_installed("shinytest2")
+  skip_if_too_deep(5)
+  app <- TealAppDriver$new(
+    data = simple_teal_data(),
+    modules = modules(
+      example_module(label = "m1"),
+      example_module(label = "m2")
+    ) |> disable_report()
+  )
 
+  expect_true(endsWith(app$get_attr(
+    selector = "#teal-teal_modules-nav-m1-add_reporter_wrapper-reporter_add-add_report_card_button",
+    attribute = "class"
+  ), "shiny-bound-input disabled"))
 
-    expect_true(endsWith(app$get_attr(
-      selector = "#teal-teal_modules-nav-m1-add_reporter_wrapper-reporter_add-add_report_card_button",
-      attribute = "class"
-    ), "shiny-bound-input disabled"))
-
-    app$stop()
-  })
-
-  testthat::it("Button is active on nested module", {
-    testthat::skip_if_not_installed("shinytest2")
-    skip_if_too_deep(5)
-    app <- TealAppDriver$new(
-      data = simple_teal_data(),
-      modules = modules(
-        example_module(label = "m1"),
-        example_module(label = "m2")
-      )
-    )
-
-    expect_true(endsWith(app$get_attr(
-      selector = "#teal-teal_modules-nav-m1-add_reporter_wrapper-reporter_add-add_report_card_button",
-      attribute = "class"
-    ), "shiny-bound-input"))
-
-    app$stop()
-  })
-
-  testthat::it("Disables button on nested modules", {
-    testthat::skip_if_not_installed("shinytest2")
-    skip_if_too_deep(5)
-    app <- TealAppDriver$new(
-      data = simple_teal_data(),
-      modules = modules(
-        example_module(label = "m1"),
-        example_module(label = "m2")
-      ) |> disable_report()
-    )
-
-    expect_true(endsWith(app$get_attr(
-      selector = "#teal-teal_modules-nav-m1-add_reporter_wrapper-reporter_add-add_report_card_button",
-      attribute = "class"
-    ), "shiny-bound-input disabled"))
-
-    app$stop()
-  })
+  app$stop()
 })
