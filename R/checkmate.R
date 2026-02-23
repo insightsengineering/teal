dbaedf2740b5a72b51253e10354ca441276f15fa
Lluís Revilla Sancho <lluis.revilla_sancho@roche.com>
2026-02-23 13:48:33 +0100

    Bring the functionality from teal.modules.general

--- R/checkmate.R
+++ R/checkmate.R
@@ -47,3 +47,68 @@ decorate_err_msg <- function(x, pre = character(0), post = character(0)) {
   )
   x
 }
+
+
+#' Internal function to check if decorators is a valid object
+#' @noRd
+check_transformators <- function(x, names = NULL, arg = "decorators") { # nolint: object_name.
+
+  check_message <- checkmate::check_list(x, names = "named")
+
+  if (!is.null(names) && isTRUE(check_message)) {
+    if (length(names(x)) != length(unique(names(x)))) {
+      check_message <- sprintf(
+        "The `%s` must contain unique names from these names: %s", arg,
+        paste(sQuote(names), collapse = ", ")
+      )
+    } else if (!all(unique(names(x)) %in% c("default", names))) {
+      check_message <- sprintf(
+        paste0(
+          "The `%s` must be a named list with:\n",
+          " * 'default' for decorating all objects and/or\n",
+          " * A name from these: %s"
+        ),
+        arg, paste(sQuote(names), collapse = ", ")
+      )
+    }
+  }
+
+  if (!isTRUE(check_message)) {
+    return(check_message)
+  }
+
+  valid_elements <- vapply(
+    x,
+    checkmate::test_class,
+    classes = "teal_transform_module",
+    FUN.VALUE = logical(1L)
+  )
+
+  # Nested list
+  if (any(!valid_elements)) {
+    valid_nested <- vapply(
+      x[!valid_elements], function(subdecorators) {
+        checks <- vapply(subdecorators,
+          checkmate::test_class,
+          classes = "teal_transform_module",
+          logical(1L)
+        )
+        all(checks)
+      },
+      FUN.VALUE = logical(1L)
+    )
+    valid_elements[!valid_elements] <- valid_nested
+  }
+
+  if (all(valid_elements)) {
+    return(TRUE)
+  }
+
+  paste0(
+    "The named list can contain a list of 'teal_transform_module' objects created ",
+    "using `teal_transform_module()` or be a `teal_transform_module` object."
+  )
+}
+#' Internal assertion on decorators
+#' @noRd
+assert_transformators <- checkmate::makeAssertionFunction(check_transformators)
