3f51362e02d3c39062a9f33ab195998052fe1751
Oriol Senan <35930244+osenan@users.noreply.github.com>
2026-02-27 12:22:11 +0100

    Report which module is in use with teal.logger (#1690)
    # Pull Request
    
    This partially will show the information on which is the current active
    module.
    Test this app, switch the teal module and verify that the logger reports
    which module we are.
    
    ```
    library(teal)
    library(teal.modules.general)
    library(teal.modules.mdr)
    library(shiny.telemetry)
    
    data <- teal_data()
    data <- within(data, {
      data <- CO2
    })
    
    logger::log_threshold(logger::INFO, namespace = "teal")
    
    app <- init(
      data = data,
      modules = modules(
        tm_data_table(
          label = "Data Table",
          variables_selected = list(data = c("Plant", "Type", "Treatment", "conc", "uptake"))
        ),
        tm_rmarkdown(
          label = "RMarkdown Module",
          rmd_content = c(
            "---",
            "title: \"R Markdown Report\"",
            "output: html_document",
            "---",
            "",
            "```{r}",
            "summary(CO2) |> print()",
            "```"
          )
        )
      )
    )
    
    shinyApp(app$ui, app$server)
    ```
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Lluís Revilla <185338939+llrs-roche@users.noreply.github.com>
    Co-authored-by: donyunardi <8597300+donyunardi@users.noreply.github.com>

--- inst/js/extendShinyJs.js
+++ inst/js/extendShinyJs.js
@@ -20,3 +20,12 @@ shinyjs.enterToSubmit = function(id, submit_id) {
     })
   );
 }
+
+// Custom message handler to get document title and send it to the server
+Shiny.addCustomMessageHandler('teal-get-document-title', function(message) {
+  const title = document.title;
+  const inputId = message.inputId;
+  console.log('Teal: Sending document title:', title, 'to input:', inputId);
+  Shiny.setInputValue(inputId, title, {priority: 'event'});
+});
+
