4d868cf136d03425aa4ba248e371065fb526c1a5
m7pr <marcin.kosinski.mk1@roche.com>
2026-04-02 12:01:19 +0200

    revert changes after merge

--- inst/js/extendShinyJs.js
+++ inst/js/extendShinyJs.js
@@ -1,26 +1,3 @@
-// This file contains functions that should be executed at the start of each session,
-// not included in the original HTML
-
-shinyjs.autoFocusModal = function(id) {
-  document.getElementById('shiny-modal').addEventListener(
-    'shown.bs.modal',
-    () => document.getElementById(id).focus(),
-    { once: true }
-  );
-}
-
-shinyjs.enterToSubmit = function(id, submit_id) {
-  document.getElementById('shiny-modal').addEventListener(
-    'shown.bs.modal',
-    () => document.getElementById(id).addEventListener('keyup', (e) => {
-      if (e.key === 'Enter') {
-        e.preventDefault(); // prevent form submission
-        document.getElementById(submit_id).click();
-      }
-    })
-  );
-}
-
 // Custom message handler to get document title and send it to the server
 Shiny.addCustomMessageHandler('teal-get-document-title', function(message) {
   const title = document.title;
